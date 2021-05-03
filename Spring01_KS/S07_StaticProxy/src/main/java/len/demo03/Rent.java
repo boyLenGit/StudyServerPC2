a025a9d5c446fb88f0d5ae557fa86940d67d398a
boyLen <1024552631@qq.com>
2021-05-03 12:24:17 +0800

    first commit

--- Spring01_KS/S07_StaticProxy/src/main/java/len/demo03/Rent.java
+++ Spring01_KS/S07_StaticProxy/src/main/java/len/demo03/Rent.java
@@ -1,10 +1,5 @@
 package len.demo03;
 
-/**
- * @author ：ltb
- * @date ：2020/7/14
- */
 public interface Rent {
-
     void rent();
 }
--- Spring01_KS/S07_StaticProxy/src/main/java/len/demo04/ProxyInvocationHandler.java
+++ Spring01_KS/S07_StaticProxy/src/main/java/len/demo04/ProxyInvocationHandler.java
@@ -1,15 +1,10 @@
-package com.kuang.demo04;
+package len.demo04;
 
-import com.kuang.demo03.Rent;
-
+import len.demo03.Rent;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-/**
- * @author ：ltb
- * @date ：2020/7/15
- */
 
 //稍后用这个类自动生成代理类！
 public class ProxyInvocationHandler implements InvocationHandler {
@@ -28,7 +23,6 @@ public class ProxyInvocationHandler implements InvocationHandler {
     }
 
     //处理代理实例并返回结果
-    @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         log(method.getName());
 
