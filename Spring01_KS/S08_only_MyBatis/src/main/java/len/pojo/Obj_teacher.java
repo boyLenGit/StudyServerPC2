f48233c502b468c6ac301c1a0e184e583cab1c33
boyLen <1024552631@qq.com>
2021-05-04 01:09:55 +0800

    first commit

---
+++ Spring01_KS/S08_only_MyBatis/src/main/java/len/pojo/Obj_teacher.java
@@ -0,0 +1,9 @@
+package len.pojo;
+
+import lombok.Data;
+
+@Data
+public class Obj_teacher {
+    private int obj_t_id;
+    private String obj_t_name;
+}
---
+++ Spring01_KS/S09_Mybatis_with_Spring/src/main/java/len/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package len.mapper;
+
+import len.pojo.User;
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> selectAllUsers();
+}
