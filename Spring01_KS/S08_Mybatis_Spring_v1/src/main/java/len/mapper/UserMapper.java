bdb12016f22d5b1961329ab623ac15d9e11c39d7
boyLen <1024552631@qq.com>
2021-05-03 23:26:24 +0800

    first commit

---
+++ Spring01_KS/S08_Mybatis_Spring_v1/src/main/java/len/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package len.mapper;
+
+import com.kuang.pojo.User;
+
+import java.util.List;
+
+/**
+ * @author ：ltb
+ * @date ：2020/7/15
+ */
+public interface UserMapper {
+
+    List<User> selectAllUsers();
+}
---
+++ Spring01_KS/S08_Mybatis_Spring_v1/src/test/java/MyTest.java
@@ -0,0 +1,25 @@
+import com.kuang.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class MyTest {
+
+    @Test
+    public void selectAllUsers() throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-mapper.xml");
+        UserMapper userMapper = context.getBean("userMapper", UserMapper.class);
+        List<User> users = userMapper.selectAllUsers();
+        users.forEach(System.out::println);
+
+    }
+}
