2097d9ec418a3bc786a86966f6cb8b3074c0a9c2
boyLen <1024552631@qq.com>
2021-05-07 17:25:48 +0800

    first commit

---
+++ Spring01_KS/S11_Affair/src/main/java/len/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package len.mapper;
+
+import len.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<User> selectUser();
+    //添加一个用户
+    Integer addUser(User user);
+
+    //删除一个用户
+    Integer deleteUser(int id);
+}
