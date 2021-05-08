1ce3f195c2d575ae318bda6161c6b54133fb8c15
boyLen <1024552631@qq.com>
2021-05-08 19:52:50 +0800

    first commit

--- SpringMVC01/S02_HelloMVC/src/main/java/len/controller/HelloController.java
+++ SpringMVC01/S02_HelloMVC/src/main/java/len/controller/HelloController.java
@@ -10,12 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloController implements Controller {
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        //ModelAndView 模型和视图
+        //ModelAndView 模型和视图。模型装数据，视图负责调转到哪里
         ModelAndView modelAndView1 = new ModelAndView();
 
         //业务代码
         // 封装对象，放在ModelAndView中。Model
         modelAndView1.addObject("msg","HelloSpringMVC!");
+
         //视图跳转
         // 封装要跳转的视图，放在ModelAndView中
         modelAndView1.setViewName("hello"); //: /WEB-INF/jsp/hello.jsp
