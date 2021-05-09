3762b571b396c7add374af6a246559148e5cadc0
boyLen <1024552631@qq.com>
2021-05-09 20:14:47 +0800

    first commit

--- SpringMVC01/S04_Controller_config/src/main/java/len/controller/EncodingController.java
+++ SpringMVC01/S04_Controller_config/src/main/java/len/controller/EncodingController.java
@@ -2,7 +2,6 @@ package len.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
