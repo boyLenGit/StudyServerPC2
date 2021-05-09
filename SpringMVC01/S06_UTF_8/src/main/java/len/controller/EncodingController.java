e62488b0fbb90a3fbbeb17b715070e51e52ddfc4
boyLen <1024552631@qq.com>
2021-05-09 17:42:15 +0800

    first commit

---
+++ SpringMVC01/S06_UTF_8/src/main/java/len/controller/EncodingController.java
@@ -0,0 +1,17 @@
+package len.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EncodingController {
+    //过滤器解决乱码
+    @RequestMapping("/t/encode")
+    public String test1(String name, Model model) {
+        System.out.println(name);
+        model.addAttribute("msg",name);
+        return "test";
+    }
+}
