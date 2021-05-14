b486319fa822d780c9d8779dd9ba355b4acb87e4
boyLen <1024552631@qq.com>
2021-05-14 20:53:09 +0800

    first commit

--- SpringMVC01/S08_Project_Book/src/test/java/TestSpring.java
+++ SpringMVC01/S08_Project_Book/src/test/java/TestSpring.java
@@ -1,8 +1,11 @@
+import len.pojo.Books;
 import len.service.BookService;
 import len.service.BookServiceImpl;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class TestSpring {
     @Test
@@ -10,6 +13,9 @@ public class TestSpring {
         ApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml");
         // 调用的是BookServiceImpl，但是类型是BookService，因为是BookService提供的接口
         BookService bookServiceImpl = (BookService) context1.getBean("BookServiceImpl");
-
+        List<Books> book1 = bookServiceImpl.queryAllBooks();
+        for (Books book:book1){
+            System.out.println(book1);
+        }
     }
 }
