8950485e1b053271d9e66456bafee09425f70ead
boyLen <1024552631@qq.com>
2021-05-14 21:42:09 +0800

    first commit

--- SpringMVC01/S08_Project_Book/src/test/java/MyTest.java
+++ SpringMVC01/S08_Project_Book/src/test/java/MyTest.java
@@ -7,23 +7,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
-public class TestSpring {
-    @Test
-    public void testBeanSQL(){
-        ApplicationContext context1 = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // 调用的是BookServiceImpl，但是类型是BookService，因为是BookService提供的接口
-        BookService bookServiceImpl = (BookService) context1.getBean("BookServiceImpl");
-        List<Books> book1 = bookServiceImpl.queryAllBooks();
-        for (Books book:book1){
-            System.out.println(book1);
-        }
-    }
-
+public class MyTest {
     @Test
     public void test() {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
         BookService bookService = (BookService)applicationContext.getBean("BookServiceImpl");
+
         List<Books> books = bookService.queryAllBooks();
+
         for (Books book : books) {
             System.out.println(book);
         }
