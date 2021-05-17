7e805fee6860abfdfa721af40f0513e701064b1a
boyLen <1024552631@qq.com>
2021-05-17 15:53:44 +0800

    first commit

---
+++ SpringMVC01/S08_Project_Book_MySQL5/src/test/java/TestMyBatis.java
@@ -0,0 +1,19 @@
+import len.dao.BookMapper;
+import len.mybatis.BooksMyBatis;
+import len.pojo.Books;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class TestMyBatis {
+    @Test
+    public void T01_getUserByID(){
+        // 获得SqlSession对象
+        SqlSession sqlSession1 = BooksMyBatis.getSqlSession();
+        BookMapper bookMapper1 = sqlSession1.getMapper(BookMapper.class);
+
+        Books books1 = bookMapper1.queryBookById(2);
+        System.out.println(books1);
+        // 关闭SQLSession
+        sqlSession1.close();
+    }
+}
