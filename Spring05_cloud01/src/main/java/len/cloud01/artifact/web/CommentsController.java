885e6a0ed805545eb86c58c08a48e495525a9edf
boyLenGit <1024552631@qq.com>
2021-12-20 00:07:17 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
@@ -1,17 +1,21 @@
 package len.cloud01.artifact.web;
 
 import len.cloud01.artifact.po.Comment;
+import len.cloud01.artifact.service.BlogService;
 import len.cloud01.artifact.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CommentsController {
     @Autowired
     private CommentService commentService;
+    @Autowired
+    private BlogService blogService;
 
     @GetMapping("/comments/{blogId}")
     public String Comments(@PathVariable Long blogId, Model model){
@@ -19,8 +23,14 @@ public class CommentsController {
         return "article :: commentList";
     }
 
-    @GetMapping("/comments")
+    // 网页提交评论内容
+    @PostMapping("/comments")
     public String post(Comment comment){
-        return "redirect:/comments/" + comment.getBlog().getId();
+        Long blogId = comment.getBlog().getId();
+        // 用于实例化Comment对象中的Blog。
+        // 【方法】到底哪些对象需要在这里单独实例化？→查看web端与Comment对象，看看哪些变量传递进去了，哪些没传递进去，没传递进去的就是需要实例化/填充的，否则Comment部分属性为空。
+        comment.setBlog(blogService.getBlog(blogId));
+        commentService.saveComment(comment);
+        return "redirect:/comments/" + blogId;
     }
 }
