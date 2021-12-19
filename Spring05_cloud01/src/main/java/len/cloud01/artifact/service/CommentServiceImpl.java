5bf9b4d84209b5d9b2f85e286a306d38b1cd1ced
boyLenGit <1024552631@qq.com>
2021-12-19 21:59:18 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/service/CommentServiceImpl.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/service/CommentServiceImpl.java
@@ -1,15 +1,22 @@
 package len.cloud01.artifact.service;
 
+import len.cloud01.artifact.dao.CommentRepository;
 import len.cloud01.artifact.po.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 public class CommentServiceImpl implements CommentService{
-
+    @Autowired
+    private CommentRepository commentRepository;
 
     @Override
     public List<Comment> listCommentByBlogId(Long blogId) {
-        return null;
+        // 作用：用于返回博客的评论，并且按照评论时间倒序排序
+        // 旧版：Sort sort = new Sort(Sort.Direction.DESC, "createTime");
+        Sort sort = Sort.by(Sort.Direction.DESC, "createTime");
+        return commentRepository.findByBlogId(blogId, sort);
     }
 
     @Override
