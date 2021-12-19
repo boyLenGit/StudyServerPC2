dd812da3454258d3e06d0ab772b1fe62bba70e22
boyLenGit <1024552631@qq.com>
2021-12-19 23:53:07 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/service/CommentServiceImpl.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/service/CommentServiceImpl.java
@@ -4,10 +4,13 @@ import len.cloud01.artifact.dao.CommentRepository;
 import len.cloud01.artifact.po.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
+@Service
 public class CommentServiceImpl implements CommentService{
     @Autowired
     private CommentRepository commentRepository;
@@ -30,6 +33,7 @@ public class CommentServiceImpl implements CommentService{
         }else {
             comment.setParentComment(null);
         }
+        comment.setCreateTime(new Date());
         return commentRepository.save(comment);
     }
 }
--- Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
@@ -1,15 +1,22 @@
 package len.cloud01.artifact.web;
 
 import len.cloud01.artifact.po.Comment;
+import len.cloud01.artifact.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CommentsController {
+    @Autowired
+    private CommentService commentService;
+
     @GetMapping("/comments/{blogId}")
-    public String Comments(@PathVariable Long blogId){
-        return "blog :: commentList";
+    public String Comments(@PathVariable Long blogId, Model model){
+        model.addAttribute("comments", commentService.listCommentByBlogId(blogId));
+        return "article :: commentList";
     }
 
     @GetMapping("/comments")
