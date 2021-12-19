dab743a5e389fabe8a7cf24c81ed4fbb3ba5d29b
boyLenGit <1024552631@qq.com>
2021-12-19 16:14:45 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
@@ -1,5 +1,6 @@
 package len.cloud01.artifact.web;
 
+import len.cloud01.artifact.po.Comment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,5 +10,10 @@ public class CommentsController {
     @GetMapping("/comments/{blogId}")
     public String Comments(@PathVariable Long blogId){
         return "blog :: commentList";
+    }
+
+    @GetMapping("/comments")
+    public String post(Comment comment){
+        return "redirect:/comments/" + comment.getBlog().getId();
     }
 }
