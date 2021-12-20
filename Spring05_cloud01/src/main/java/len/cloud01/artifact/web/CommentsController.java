6a28924b87becdaddc2d7af19a54e6ada1727575
boyLenGit <1024552631@qq.com>
2021-12-20 20:55:45 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
@@ -17,6 +17,7 @@ public class CommentsController {
     private CommentService commentService;
     @Autowired
     private BlogService blogService;
+    // ↓ 这个comment.avatar位于/resources/application-dev.yml
     @Value("comment.avatar")
     private String avatar;
 
@@ -33,6 +34,7 @@ public class CommentsController {
         // 用于实例化Comment对象中的Blog。
         // 【方法】到底哪些对象需要在这里单独实例化？→查看web端与Comment对象，看看哪些变量传递进去了，哪些没传递进去，没传递进去的就是需要实例化/填充的，否则Comment部分属性为空。
         comment.setBlog(blogService.getBlog(blogId));
+        comment.setAvatar(avatar);
         commentService.saveComment(comment);
         return "redirect:/comments/" + blogId;
     }
