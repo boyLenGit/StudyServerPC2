ba8eb4b3698bb13605c45b0019da5c00eac1c5bd
boyLenGit <1024552631@qq.com>
2021-12-21 21:39:16 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
@@ -3,6 +3,7 @@ package len.cloud01.artifact.web;
 import len.cloud01.artifact.po.Comment;
 import len.cloud01.artifact.service.BlogService;
 import len.cloud01.artifact.service.CommentService;
+import len.cloud01.artifact.util.base.LenLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -30,6 +31,7 @@ public class CommentsController {
     // 网页提交评论内容
     @PostMapping("/comments")
     public String post(Comment comment){
+        LenLog.staticInfo("Comment-post", comment.toString());
         Long blogId = comment.getBlog().getId();
         // 用于实例化Comment对象中的Blog。
         // 【方法】到底哪些对象需要在这里单独实例化？→查看web端与Comment对象，看看哪些变量传递进去了，哪些没传递进去，没传递进去的就是需要实例化/填充的，否则Comment部分属性为空。
