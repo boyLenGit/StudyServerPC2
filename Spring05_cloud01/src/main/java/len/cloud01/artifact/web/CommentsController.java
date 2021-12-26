1b3938ab13259088815f50ac0d5f6815a8f085f8
boyLenGit <1024552631@qq.com>
2021-12-26 19:10:45 +0800

    first commit

--- Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
+++ Spring05_cloud01/src/main/java/len/cloud01/artifact/web/CommentsController.java
@@ -1,6 +1,8 @@
 package len.cloud01.artifact.web;
 
+import len.cloud01.artifact.po.Blog;
 import len.cloud01.artifact.po.Comment;
+import len.cloud01.artifact.po.User;
 import len.cloud01.artifact.service.BlogService;
 import len.cloud01.artifact.service.CommentService;
 import len.cloud01.artifact.util.base.LenLog;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -34,13 +37,24 @@ public class CommentsController {
 
     // 网页提交评论内容
     @PostMapping("/comments")
-    public String post(Comment comment){
+    public String post(Comment comment, HttpSession session){
         Long blogId = comment.getBlog().getId();
         // 用于实例化Comment对象中的Blog。
         // 【方法】到底哪些对象需要在这里单独实例化？→查看web端与Comment对象，看看哪些变量传递进去了，哪些没传递进去，没传递进去的就是需要实例化/填充的，否则Comment部分属性为空。
         comment.setBlog(blogService.getBlog(blogId));
-        comment.setAvatar(avatar);
-        comment.setAdminComment(false);
+        // ↓ 管理员身份验证
+        Blog blog = (Blog) session.getAttribute("blog");
+        User user = blog.getUser();
+        LenLog.staticInfo("Comment-post", user.getUsername() + "||" + comment.getNickname());
+        if (user.getUsername().equals(comment.getNickname())){
+            // 表明评论用户就是博客作者
+            comment.setAvatar(avatar);
+            comment.setAdminComment(true);  // 设置comment用户为作者
+        }else {
+            comment.setAvatar(avatar);
+            comment.setAdminComment(false);
+        }
+        // ↓ 保存comment
         commentService.saveComment(comment);
         return "redirect:/comments/" + blogId;
     }
