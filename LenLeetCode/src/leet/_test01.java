4d884a37469240f7b400382ce5c36c7a80c188fe
boyLenGit <1024552631@qq.com>
2021-12-29 21:15:33 +0800

    first commit

--- LenLeetCode/src/leet/_test01.java
+++ LenLeetCode/src/leet/_test01.java
@@ -1,4 +1,14 @@
 package leet;
 
+import util.LenLog;
+
+import java.util.Arrays;
+
 public class _test01 {
+    public static void main(String[] args) {
+        //
+        l001 loo1 = new l001();
+        int[] result = loo1.twoSum(new int[]{3,2,4},6);
+        LenLog.printString(Arrays.toString(result));
+    }
 }
