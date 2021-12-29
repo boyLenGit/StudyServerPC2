f0dd60e652bef89ebc58b0d83ce650fe6fc265f8
boyLenGit <1024552631@qq.com>
2021-12-30 00:10:22 +0800

    first commit

--- LenLeetCode/src/leet/l002.java
+++ LenLeetCode/src/leet/l002.java
@@ -3,15 +3,20 @@ package leet;
 import leet.help.ListNode;
 
 public class l002 {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         String string1 = "";
         String string2 = "";
+
         while (true){
             string1 = string1.concat(Integer.toString(l1.val));
             if (l1.next==null){
                 break;
+            }else {
+                l1 = l1.next;
             }
         }
         Long num1 = Long.valueOf(string1);
+
+        return null;
     }
 }
