fac0d95942e8223af3c89ed42d48dee6246def8b
boyLenGit <1024552631@qq.com>
2021-12-30 00:12:31 +0800

    first commit

--- LenLeetCode/src/leet/_test01.java
+++ LenLeetCode/src/leet/_test01.java
@@ -1,14 +1,13 @@
 package leet;
 
+import leet.help.ListNode;
 import util.LenLog;
 
 import java.util.Arrays;
 
 public class _test01 {
     public static void main(String[] args) {
-        //
-        l001 loo1 = new l001();
-        int[] result = loo1.twoSum(new int[]{3,2,4},6);
-        LenLog.printString(Arrays.toString(result));
+        ListNode l1 = new ListNode(2, new ListNode(3, new ListNode(4, null)));
+        l002.addTwoNumbers(l1, l1);
     }
 }
