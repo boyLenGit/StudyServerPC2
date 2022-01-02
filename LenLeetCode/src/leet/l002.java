c4bc53479cb8103a2ae40d134872667a4b369d02
boyLenGit <1024552631@qq.com>
2022-01-02 23:07:26 +0800

    first commit

--- LenLeetCode/src/leet/l002.java
+++ LenLeetCode/src/leet/l002.java
@@ -41,28 +41,28 @@ public class l002 {
         }
         // 开始合并结果
         //      String实现大数相加算法
-        char[] chars_add_1 = string1.toCharArray();
+        char[] chars_add_1 = string1.toCharArray(); // 将大数String转化为Char数组
         char[] chars_add_2 = string2.toCharArray();
-        String string_add_result = "";
-        int index_1 = string1.length()-1;
-        int index_2 = string2.length()-1;
-        int num_adder_1 = 0;
-        int num_adder_2 = 0;
-        char[] chars_add_result;
-        int add_next_level = 0;
+        String string_add_result = ""; // 用于储存最后的大数相加结果
+        int index_1 = string1.length()-1; // chars_add_1的索引，用于倒序遍历chars_add_1中的数值
+        int index_2 = string2.length()-1; // chars_add_2的索引
+        int num_adder_1 = 0; // 抽出的chars_add_1中的末尾数字
+        int num_adder_2 = 0; // 抽出的chars_add_2中的末尾数字
+        int add_next_level = 0; // 进位判断变量，当=0时不需要进位，当=1时需要进位
         while (true){
-            if(index_1<0){
+            // 抽出最末尾的数字，进行相加
+            if(index_1<0){ // 处理chars_add_1遍历完的情况
                 num_adder_1 = 0;
             }else {
                 num_adder_1 = chars_add_1[index_1] - '0';
             }
-            if(index_1<0){
+            if(index_2<0){
                 num_adder_2 = 0;
             }else {
                 num_adder_2 = chars_add_2[index_2] - '0';
             }
-
             int num_adder_result = num_adder_1 + num_adder_2;
+            // 若相加的和>=10，需要进位
             if(add_next_level == 1){
                 num_adder_result = num_adder_result + 1;
                 add_next_level = 0;
@@ -72,9 +72,15 @@ public class l002 {
                 num_adder_result = num_adder_result - 10;
             }
             string_add_result = string_add_result.concat(String.valueOf(num_adder_result));
+            // while循环停止的条件：当两个chars都遍历结束后
             if(index_1<=0 && index_2<=0){
+                // 如果遍历结束，还有进位，则进行进位操作，防止出现999+1=0000的结果
+                if(add_next_level==1){
+                    string_add_result = string_add_result.concat("1");
+                }
                 break;
             }
+            // 索引值每次循环都-1，用于倒序遍历char[]
             index_1--;
             index_2--;
         }
@@ -95,5 +101,36 @@ public class l002 {
             listNode_result = new ListNode((int) (chars_result[i4]-'0'), listNode_result);
         }
         return listNode_result;
+    }
+
+
+    public static ListNode addTwoNumbers_lc_user(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);   //创建首节点
+        ListNode cursor = root;  //声名一个变量用来在移动过程中指向当前节点
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            //获取链表的值
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            //计算和
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+            //生成新的节点
+            ListNode sumNode = new ListNode(sumVal % 10);
+            //移动指针指向下一个值
+            cursor.next = sumNode;  //把新节点连起来
+            cursor = sumNode;  //当前节点往后移动
+            if(l1 != null) l1 = l1.next;  //将节点向后移动，对l1的下一个节点进行处理
+            if(l2 != null) l2 = l2.next;  //将节点向后移动，对l2的下一个节点进行处理
+        }
+        return root.next;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(3, new ListNode(4, null)));
+        ListNode l2 = new ListNode(1, new ListNode(2, new ListNode(3, null)));
+        ListNode l3 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(1, new ListNode(5, null)))));
+        l002.addTwoNumbers_lc_user(l1, l3);
     }
 }
