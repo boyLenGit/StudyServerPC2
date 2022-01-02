228b6a5856c23f4532246807f456b30d60dc255e
boyLenGit <1024552631@qq.com>
2022-01-02 14:43:05 +0800

    first commit

--- LenLeetCode/src/leet/l002.java
+++ LenLeetCode/src/leet/l002.java
@@ -24,8 +24,7 @@ public class l002 {
         for(int i1=chars1.length-1; i1>=0; i1--){
             string1 = string1.concat(String.valueOf(chars1[i1]));
         }
-        BigInteger bigInteger1 = BigInteger.valueOf(Long.parseLong(string1));
-//        Long num1 = Long.valueOf(string1);
+//        BigInteger bigInteger1 = BigInteger.valueOf(Long.parseLong(string1));
 
         // l2
         while (true){
@@ -41,16 +40,32 @@ public class l002 {
         for(int i2=chars2.length-1; i2>=0; i2--){
             string2 = string2.concat(String.valueOf(chars2[i2]));
         }
-        BigInteger bigInteger2 = BigInteger.valueOf(Long.parseLong(string2));
+//        BigInteger bigInteger2 = BigInteger.valueOf(Long.parseLong(string2));
         // 开始合并结果
-        BigInteger bigInteger_result = bigInteger1.add(bigInteger2);
+        //      String实现大数相加算法
+        char[] chars_add_1 = string1.toCharArray();
+        char[] chars_add_2 = string2.toCharArray();
+        int index_1 = string1.length()-1;
+        int index_2 = string2.length()-1;
+        char[] chars_add_result;
+        int add_next_level = 0;
+        while (true){
+            int num_adder_1 = chars_add_1[index_1] - '0';
+            int num_adder_2 = chars_add_2[index_2] - '0';
+            int num_adder_result = num_adder_1 + num_adder_2;
+            if(num_adder_result>=10){
+                add_next_level = 1;
+                num_adder_result = num_adder_result - 10;
+            }
+            
+            if(index_1<=0 && index_2<=0){
+                break;
+            }
+            index_1--;
+            index_2--;
+            add_next_level = 0;
+        }
         char[] chars_result = String.valueOf(bigInteger_result).toCharArray();
-        //      反转数组
-//        String string_result = "";
-//        for(int i_r=chars_result.length-1; i_r>=0; i_r--){
-//            string_result = string_result.concat(String.valueOf(chars_result[i_r]));
-//        }
-//        chars_result = string_result.toCharArray();
         // result to ListNode
         ListNode listNode_result = new ListNode();
         for(int i4=0; i4<chars_result.length; i4++){
