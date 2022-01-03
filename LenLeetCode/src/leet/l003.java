1ce6aadb5f2eadd196197d9d719dac8f310a7c72
boyLenGit <1024552631@qq.com>
2022-01-03 15:12:43 +0800

    first commit

--- LenLeetCode/src/leet/l003.java
+++ LenLeetCode/src/leet/l003.java
@@ -2,6 +2,17 @@ package leet;
 
 public class l003 {
     public static int lengthOfLongestSubstring(String s) {
-        
+        char[] chars_input = s.toCharArray();
+        int st_length = chars_input.length;
+        int dy_length = chars_input.length;
+        while (true){
+            for(int i1=0; i1<(int)st_length/dy_length; i1++){
+                // 遍历每一组的chars
+                for(int i2=i1; i2<dy_length; i2++){
+                    
+                }
+            }
+            dy_length--;
+        }
     }
 }
