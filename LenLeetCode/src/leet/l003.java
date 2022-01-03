0fcd978732a39062293d8795b5c045a4f4c197d5
boyLenGit <1024552631@qq.com>
2022-01-03 17:47:06 +0800

    first commit

--- LenLeetCode/src/leet/l003.java
+++ LenLeetCode/src/leet/l003.java
@@ -15,21 +15,25 @@ public class l003 {
             if(dy_length<=0){
                 return 0;
             }
+            int for_time = (st_length%dy_length)==0 ? st_length/dy_length : (st_length/dy_length)+1;
             // 遍历某一长度下每一组的chars
-            for(int i1=0; i1<(int)st_length/dy_length; i1++){
+            for(int i1=0; i1<for_time; i1++){
                 // 遍历每一组的单个chars
                 for(int i2=i1; i2<dy_length+i1; i2++){
-                    for(int i2_2=i2; i2_2<dy_length+i1; i2_2++){
+                    for(int i2_2=i2+1; i2_2<dy_length+i1; i2_2++){
                         // 表明有重复
                         if(chars_input[i2]==chars_input[i2_2]){
                             isRepeat = 1;
                             break;
                         }
                     }
+                    if(isRepeat==1){
+                        break;
+                    }
                 }
                 // 如果循环完了没有重复的，则表明是最大非重复序列
                 if (isRepeat==0){
-                    return i1;
+                    return dy_length;
                 }
                 isRepeat = 0;
             }
@@ -38,7 +42,7 @@ public class l003 {
     }
 
     public static void main(String[] args) {
-        int result = lengthOfLongestSubstring("abcabcbb");
+        int result = lengthOfLongestSubstring("pwwkew");
         System.out.println(result);
     }
 }
