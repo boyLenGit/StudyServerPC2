3a410c56e3524375e5ad969f7d90144c6aa689f5
boyLenGit <1024552631@qq.com>
2022-01-03 17:28:44 +0800

    first commit

--- LenLeetCode/src/leet/l003.java
+++ LenLeetCode/src/leet/l003.java
@@ -1,5 +1,7 @@
 package leet;
 
+import leet.help.ListNode;
+
 public class l003 {
     public static int lengthOfLongestSubstring(String s) {
         char[] chars_input = s.toCharArray();
@@ -7,24 +9,36 @@ public class l003 {
         int dy_length = chars_input.length;
         int isRepeat = 0; // 1则重复，0则无重复
         while (true){
+            if(dy_length==1){
+                return 1;
+            }
+            if(dy_length<=0){
+                return 0;
+            }
+            // 遍历某一长度下每一组的chars
             for(int i1=0; i1<(int)st_length/dy_length; i1++){
-                // 遍历每一组的chars
-                char[] chars_temp;
-                String string_temp;
-                // 提取出
-                for(int i2=i1; i2<dy_length; i2++){
-                    // 表明有重复
-                    if(chars_input[i1]==chars_input[i2]){
-                        isRepeat=1;
-                        break;
+                // 遍历每一组的单个chars
+                for(int i2=i1; i2<dy_length+i1; i2++){
+                    for(int i2_2=i2; i2_2<dy_length+i1; i2_2++){
+                        // 表明有重复
+                        if(chars_input[i2]==chars_input[i2_2]){
+                            isRepeat = 1;
+                            break;
+                        }
                     }
                 }
                 // 如果循环完了没有重复的，则表明是最大非重复序列
                 if (isRepeat==0){
-                    return 
+                    return i1;
                 }
+                isRepeat = 0;
             }
             dy_length--;
         }
+    }
+
+    public static void main(String[] args) {
+        int result = lengthOfLongestSubstring("abcabcbb");
+        System.out.println(result);
     }
 }
