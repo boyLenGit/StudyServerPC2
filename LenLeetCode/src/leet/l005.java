ebd03e58fba28c172b3f98af5eae084f00c69ed9
boyLenGit <1024552631@qq.com>
2022-01-04 17:06:48 +0800

    first commit

--- LenLeetCode/src/leet/l005.java
+++ LenLeetCode/src/leet/l005.java
@@ -11,12 +11,25 @@ public class l005 {
         int length = s.length();
         int[] input_list = new int[length];
         Arrays.fill(input_list, 0);
-        for(int i=0; i<length; i++){
-            int char_raw = s.charAt(i);
-            int indexHwArea_temp = char_table[char_raw]!= -1 ? char_table[char_raw]+1 : indexHwArea;
-            indexHwArea = Math.max(indexHwArea_temp, indexHwArea);
-            input_list[i] = char_table[char_raw]!= -1 ? -char_raw : char_raw;
-            
+        for(int i_0=0; i_0<length; i_0++){
+            int char_raw = s.charAt(i_0);
+            // 偶数情况
+            if(i_0+1<length || s.charAt(i_0)==s.charAt(i_0+1)){
+                int pointReverse = 0;
+                for(int i_1=i_0+2; i_1<length; i_1++){
+                    pointReverse = i_0 - (i_1 - i_0);
+                    if(pointReverse<0){
+                        break;
+                    }
+                    // 如果想上遍历还是回文
+                    if(s.charAt(pointReverse)==s.charAt(i_1))
+                    return "";
+                }
+            }
+
+
+
+
 
         }
         return null;
