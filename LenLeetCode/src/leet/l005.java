e0335d939c661e4488a745addb8f5e5cefbba8ec
boyLenGit <1024552631@qq.com>
2022-01-04 18:58:40 +0800

    first commit

--- LenLeetCode/src/leet/l005.java
+++ LenLeetCode/src/leet/l005.java
@@ -14,20 +14,19 @@ public class l005 {
             // 偶数情况
             if(i_0+1<length){
                 if(s.charAt(i_0)==s.charAt(i_0+1)){
-                    int pointReverse = 0;
-                    int indexHwStart = 0;
-                    int indexHwEnd = 0;
+                    int indexHwStart = i_0;
+                    int indexHwEnd = i_0 + 1;
+                    lengthHw_temp = 2;
                     for(int i_1=i_0+2; i_1<length; i_1++){
-                        pointReverse = i_0 - (i_1 - i_0) + 1;
+                        indexHwStart = i_0 - (i_1 - i_0) + 1;
                         // 如果索引越界了
-                        if(pointReverse<0){
+                        if(indexHwStart<0){
                             break;
                         }
                         // 如果向上遍历不是回文
-                        if(s.charAt(pointReverse)!=s.charAt(i_1)){
+                        if(s.charAt(indexHwStart)!=s.charAt(i_1)){
                             break;
                         }
-                        indexHwStart = pointReverse;
                         indexHwEnd = i_1;
                         lengthHw_temp = 2 * (i_1 - i_0);
                     }
@@ -38,13 +37,45 @@ public class l005 {
                 }
             }
 
+            // 奇数情况
+            if(i_0+1<length||i_0-1>=0){
+                if(s.charAt(i_0-1)==s.charAt(i_0+1)){
+                    int indexHwStart = i_0 - 1;
+                    int indexHwEnd = i_0 + 1;
+                    lengthHw_temp = 3;
+                    for(int i_1=i_0+2; i_1<length; i_1++){
+                        indexHwStart = i_0 - (i_1 - i_0);
+                        indexHwEnd = i_1;
+                        // 如果索引越界了
+                        if(indexHwStart<0){
+                            break;
+                        }
+                        // 如果向上遍历不是回文
+                        if(s.charAt(indexHwStart)!=s.charAt(indexHwEnd)){
+                            break;
+                        }
+
+                        lengthHw_temp = 2 * (i_1 - i_0);
+                    }
+                    if(lengthHw_temp>lengthHw){
+                        lengthHw = lengthHw_temp;
+                        result = s.substring(indexHwStart, indexHwEnd+1);
+                    }
+                }
+            }
+
+        }
+        LenLog.staticInfo("longestPalindrome", String.valueOf(lengthHw));
+
+        if (lengthHw<=0){
+            result = s.substring(0,1);
         }
         return result;
     }
 
 
     public static void main(String[] args) {
-        String test01 = "xnoonxy";
+        String test01 = "22e11";
         String result = longestPalindrome(test01);
         LenLog.printString(result);
     }
