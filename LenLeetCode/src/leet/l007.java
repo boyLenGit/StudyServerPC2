8aabbc1acc25dc08d5e819eb54206a784f34d5ae
boyLenGit <1024552631@qq.com>
2022-01-06 00:24:08 +0800

    first commit

--- LenLeetCode/src/leet/l007.java
+++ LenLeetCode/src/leet/l007.java
@@ -2,6 +2,16 @@ package leet;
 
 public class l007 {
     public static int reverse(int x) {
-        int isUnder = x>=0 ? 0 : 1;
+        int isPositive = x>=0 ? 1 : 0;
+        String string_input = String.valueOf(Math.abs(x));
+        char[] chars_input = string_input.toCharArray();
+        char[] chars_result = string_input.toCharArray();
+        int length = chars_input.length;
+        for(int i0=0; i0<length; i0++){
+            chars_result[length -1 - i0] = chars_input[i0];
+        }
+        String string_result = String.valueOf(chars_result);
+        int int_result = Integer.parseInt(string_result);
+        return int_result;
     }
 }
