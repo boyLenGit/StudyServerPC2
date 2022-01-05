eb618a9f9912e30b7481ce5c4c5f3e8cd17b98f2
boyLenGit <1024552631@qq.com>
2022-01-05 14:11:08 +0800

    first commit

--- LenLeetCode/src/leet/l006.java
+++ LenLeetCode/src/leet/l006.java
@@ -1,48 +1,67 @@
 package leet;
 
+import util.LenLog;
+
 import java.util.Arrays;
 
 public class l006 {
     public static String convert(String s, int numRows) {
         char[][] chars_2v = new char[numRows][];
         int length = s.length();
-        int length_s_2v = (s.length()/(numRows+1)) * 2;
+        int length_s_2v = s.length();
         // 生成全是0的二维矩阵
         for(int i1=0; i1<numRows; i1++){
             char[] chars_temp = new char[length_s_2v];
             Arrays.fill(chars_temp, '0');
             chars_2v[i1] = chars_temp;
         }
+        // 竖向生成内容
         int index_1v = 0;
         int index_2v = 0;
-        int single_row_index_1v = numRows -1 - 1;
-        int single_row_checker = 0;
         for(int i0=0; i0<length; i0++){
             // index_1v与index_2v更新
             if(index_1v>=numRows){
                 index_1v=0;
                 index_2v++;
-                single_row_checker = single_row_checker!=0 ? 0 : 1;
-            }
-            // single_row_index_1v值检查
-            if (single_row_index_1v<=0){
-                single_row_index_1v = numRows -1 - 1;
+
+                // 如果是要保存单列的char
+                for (int i2=numRows-1-1; i2>0; i2--){
+                    chars_2v[i2][index_2v] = s.charAt(i0);
+                    index_2v++;
+                    i0++;
+                    if(i0>=length){
+                        break;
+                    }
+                }
             }
             // 保存列的内容
-            if (single_row_checker==1){ // 如果是要保存单列的char
-                chars_2v[single_row_index_1v][index_2v] = s.charAt(i0);
-                index_2v++;
-                single_row_checker = 0;
-            }else { // 常规列保存
-                chars_2v[index_1v][index_2v] = s.charAt(i0);
+            if(i0>=length){  // 检查防止溢出
+                break;
             }
+            chars_2v[index_1v][index_2v] = s.charAt(i0);
             index_1v++;
         }
-        return s;
+
+        // 将生成的竖向数组进行横向读取，变为Z字形字符串
+        String result_temp = "";
+        for (char[] chars : chars_2v) {
+            result_temp = result_temp.concat(String.valueOf(chars));
+        }
+        // 剔除包含0的char
+        char[] chars_result_temp = result_temp.toCharArray();
+        String result = "";
+        for (char c : chars_result_temp) {
+            if (c == '0') {
+                continue;
+            }
+            result = result.concat(String.valueOf(c));
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        String test01 = "PAYPALISHIRING";
+        String test01 = "ABCD";
         String result = convert(test01, 3);
+        LenLog.printString(result);
     }
 }
