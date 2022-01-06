8373b3e3d5c0faf2dab99bb995b3fdef4b03b999
boyLenGit <1024552631@qq.com>
2022-01-07 00:10:07 +0800

    first commit

--- LenLeetCode/src/leet/l008.java
+++ LenLeetCode/src/leet/l008.java
@@ -1,5 +1,7 @@
 package leet;
 
+import util.LenLog;
+
 public class l008 {
     public static int myAtoi(String s) {
         char[] chars_input = s.toCharArray();
@@ -7,23 +9,28 @@ public class l008 {
         int isFront = 1;
         String string_result = "";
         for(int i1=0; i1<chars_input.length; i1++){
-            if (chars_input[i1]==' '){
-                continue;
-            }
-            if (chars_input[i1] == '-') {
+
+            if (chars_input[i1] == '-' & i1+1<chars_input.length) {
+                if (chars_input[i1+1]>='0' & chars_input[i1+1]<='9')
                 isPositive = 0;
                 continue;
             }
+            if (chars_input[i1]>='0' & chars_input[i1]<='9')
             string_result = string_result.concat(String.valueOf(chars_input[i1]));
         }
         Long long_result = Long.parseLong(string_result);
-        if ((isPositive==1 & long_result>Long.parseLong("2147483647")) | (isPositive==0 & long_result>Long.parseLong("2147483648"))){
-            return 0;
+        if (isPositive==1 & long_result>Long.parseLong("2147483647")){
+            return 2147483647;
         }
-        return 0;
+        if (isPositive==0 & long_result>Long.parseLong("2147483648")){
+            return -2147483648;
+        }
+
+        return isPositive==1 ? Integer.parseInt(string_result) : Integer.parseInt(string_result) * (-1);
     }
 
     public static void main(String[] args) {
-        myAtoi("   -42");
+        int result = myAtoi("-4193 with -words");
+        LenLog.printString(String.valueOf(result));
     }
 }
