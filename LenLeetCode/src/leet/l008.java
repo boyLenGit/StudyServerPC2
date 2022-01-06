fa1b51c7b5746a062a6a1dda0ed356689d9f9554
boyLenGit <1024552631@qq.com>
2022-01-06 23:57:25 +0800

    first commit

--- LenLeetCode/src/leet/l008.java
+++ LenLeetCode/src/leet/l008.java
@@ -5,11 +5,25 @@ public class l008 {
         char[] chars_input = s.toCharArray();
         int isPositive = 1;
         int isFront = 1;
-        for(char char_input: chars_input){
-            if (char_input==' '){
+        String string_result = "";
+        for(int i1=0; i1<chars_input.length; i1++){
+            if (chars_input[i1]==' '){
                 continue;
             }
-
+            if (chars_input[i1] == '-') {
+                isPositive = 0;
+                continue;
+            }
+            string_result = string_result.concat(String.valueOf(chars_input[i1]));
         }
+        Long long_result = Long.parseLong(string_result);
+        if ((isPositive==1 & long_result>Long.parseLong("2147483647")) | (isPositive==0 & long_result>Long.parseLong("2147483648"))){
+            return 0;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        myAtoi("   -42");
     }
 }
