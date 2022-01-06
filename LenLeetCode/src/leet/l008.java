9edb6fd7114ad7a39ce7cf3f7e84e4dc539809db
boyLenGit <1024552631@qq.com>
2022-01-07 00:44:08 +0800

    first commit

--- LenLeetCode/src/leet/l008.java
+++ LenLeetCode/src/leet/l008.java
@@ -4,19 +4,20 @@ import util.LenLog;
 
 public class l008 {
     public static int myAtoi(String s) {
-        char[] chars_input = s.toCharArray();
-        int isPositive = 1;
-        int isFront = 1;
+        char[] chars_input = s.trim().toCharArray();
+        int isPositive = chars_input[0]=='-' ? 0 : 1;
+        int isfirstWord = 1;
         String string_result = "";
         for(int i1=0; i1<chars_input.length; i1++){
-
-            if (chars_input[i1] == '-' & i1+1<chars_input.length) {
-                if (chars_input[i1+1]>='0' & chars_input[i1+1]<='9')
-                isPositive = 0;
-                continue;
+            if (chars_input[i1]==' '){
+                break;
             }
             if (chars_input[i1]>='0' & chars_input[i1]<='9')
             string_result = string_result.concat(String.valueOf(chars_input[i1]));
+        }
+
+        if (string_result.length()==0){
+            return 0;
         }
         Long long_result = Long.parseLong(string_result);
         if (isPositive==1 & long_result>Long.parseLong("2147483647")){
@@ -30,7 +31,7 @@ public class l008 {
     }
 
     public static void main(String[] args) {
-        int result = myAtoi("-4193 with -words");
+        int result = myAtoi("-91283472332");
         LenLog.printString(String.valueOf(result));
     }
 }
