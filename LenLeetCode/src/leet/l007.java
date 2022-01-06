1033a5835c8dcb99553ee1657e0a8eb8e4877922
boyLenGit <1024552631@qq.com>
2022-01-06 15:46:17 +0800

    first commit

--- LenLeetCode/src/leet/l007.java
+++ LenLeetCode/src/leet/l007.java
@@ -4,7 +4,7 @@ import util.LenLog;
 
 public class l007 {
     public static int reverse(int x) {
-        String string_input = String.valueOf(Math.abs(x));
+        String string_input = x>=0 ? String.valueOf(x) : String.valueOf(x).substring(1, String.valueOf(x).length());
         char[] chars_input = string_input.toCharArray();
         char[] chars_result = string_input.toCharArray();
         int length = chars_input.length;
@@ -16,13 +16,14 @@ public class l007 {
         if ((x>=0 & long_result>Long.parseLong("2147483647")) | (x<0 & long_result>Long.parseLong("2147483648"))){
             return 0;
         }
-        int int_result = Integer.parseInt(string_result);
+        int int_result = x>=0 ? Integer.parseInt(string_result) : Integer.parseInt(string_result) * (-1);
         return x>=0 ? int_result : (-1 * int_result);
     }
 
     public static void main(String[] args) {
-        int input = 1534236469;
-        int result = reverse(input);
+        int input1 = 1534236469;
+        int input2 = -2147483648;
+        int result = reverse(input2);
         LenLog.printString(String.valueOf(result));
     }
 }
