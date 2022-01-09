b06934e2ddc567a0523ce7dc3ad6492889a4e87b
boyLenGit <1024552631@qq.com>
2022-01-09 19:54:04 +0800

    first commit

--- LenLeetCode/src/leet/l012.java
+++ LenLeetCode/src/leet/l012.java
@@ -38,43 +38,24 @@ public class l012 {
     public static String intToRoman_len2(int num) {
         ArrayList<Integer> divisors = new ArrayList<Integer>(Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1));
         ArrayList<String> romas = new ArrayList<String>(Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"));
-        ArrayList<Integer> devide_result = new ArrayList<Integer>();
-        // 计算每个罗马数字出现的次数
-        int be_divisor = num;
-        for (int divisor : divisors) {
-            if (divisor > be_divisor) { // 如果输入值＞被除数，则跳过
-                devide_result.add(0);
-                continue;
-            } else {
-                devide_result.add(be_divisor / divisor);
-                be_divisor = be_divisor % divisor;
-
-            }
-        }
-        for (int i1=0; i1<divisors.size(); i1++){
-            if (divisors.get(i1)>be_divisor){
-                devide_result.add(0);
-                continue;
-            }else {
-                devide_result.add(be_divisor / divisors.get(i1));
-                be_divisor = be_divisor % divisors.get(i1);
-                
-            }
-        }
-        // 生成罗马数字的结果
         String result = "";
-        for (int i1=0; i1<devide_result.size(); i1++){
-            if (devide_result.get(i1)!=0){
-                for (int i2=0; i2<devide_result.get(i1); i2++){
+        int be_divisor = num;
+        for (int i1=0; i1<divisors.size(); i1++){
+            if (divisors.get(i1)<=be_divisor){
+                // 生成罗马数字的结果
+                for (int i2=0; i2<be_divisor / divisors.get(i1); i2++){
                     result = result.concat(romas.get(i1));
                 }
+                be_divisor = be_divisor % divisors.get(i1);
             }
         }
         return result;
     }
+    // 耗时和内存消耗没什么提升
+
 
     public static void main(String[] args) {
-        String result = intToRoman(1994);
+        String result = intToRoman_len2(1);
         LenLog.printString(result);
     }
 }
