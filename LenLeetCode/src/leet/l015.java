67592583d7a6b41d55bf52b7e62207334b403c36
boyLenGit <1024552631@qq.com>
2022-01-10 19:39:03 +0800

    first commit

--- LenLeetCode/src/leet/l015.java
+++ LenLeetCode/src/leet/l015.java
@@ -7,22 +7,18 @@ import java.util.stream.Collectors;
 
 public class l015 {
     public static List<List<Integer>> threeSum(int[] nums) {
-        int pointer1 = 0;
-        int pointer2 = 0;
+        if (nums.length==0 | nums.length==1) return new ArrayList<List<Integer>>();
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.parallelSort(nums);
-        int zero_position = Arrays.binarySearch(nums, 0);
         List<Integer> inputs = Arrays.stream(nums).boxed().collect(Collectors.toList());
-//        for(int i1=0, i2=inputs.size()-1; i1<i2; i1++, i2--){
-//
-//        }
         int sum;
         for (int i1=0; i1<inputs.size(); i1++){
             if (inputs.get(i1)>0) break;
-            for (int i2=i1+1, i3=inputs.size()-1; i2<inputs.size(); ){
+            for (int i2=i1+1, i3=inputs.size()-1; i2<i3; ){
                 sum = inputs.get(i1) + inputs.get(i2) + inputs.get(i3);
                 if (sum==0){
                     result.add(new ArrayList<>(Arrays.asList(inputs.get(i1), inputs.get(i2), inputs.get(i3))));
+                    break;
                 }else {
                     if (sum>0){
                         i3--;
@@ -34,10 +30,6 @@ public class l015 {
                 }
             }
         }
-
-
-        result.add(new ArrayList<>(Arrays.asList(-1,0,1)));
-        result.add(new ArrayList<>(Arrays.asList(-1,-1,2)));
         return result;
     }
 
