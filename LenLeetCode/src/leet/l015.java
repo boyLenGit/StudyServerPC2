7812c3e7ce81ffd4f641aa979b01dd306cf8c1a3
boyLenGit <1024552631@qq.com>
2022-01-10 18:56:34 +0800

    first commit

--- LenLeetCode/src/leet/l015.java
+++ LenLeetCode/src/leet/l015.java
@@ -11,8 +11,29 @@ public class l015 {
         int pointer2 = 0;
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         Arrays.parallelSort(nums);
+        int zero_position = Arrays.binarySearch(nums, 0);
         List<Integer> inputs = Arrays.stream(nums).boxed().collect(Collectors.toList());
-
+//        for(int i1=0, i2=inputs.size()-1; i1<i2; i1++, i2--){
+//
+//        }
+        int sum;
+        for (int i1=0; i1<inputs.size(); i1++){
+            if (inputs.get(i1)>0) break;
+            for (int i2=i1+1, i3=inputs.size()-1; i2<inputs.size(); ){
+                sum = inputs.get(i1) + inputs.get(i2) + inputs.get(i3);
+                if (sum==0){
+                    result.add(new ArrayList<>(Arrays.asList(inputs.get(i1), inputs.get(i2), inputs.get(i3))));
+                }else {
+                    if (sum>0){
+                        i3--;
+                        continue;
+                    }else {
+                        i2++;
+                        continue;
+                    }
+                }
+            }
+        }
 
 
         result.add(new ArrayList<>(Arrays.asList(-1,0,1)));
@@ -25,3 +46,6 @@ public class l015 {
         System.out.println(result);
     }
 }
+
+// -4 -1 -1 0 1 2
+
