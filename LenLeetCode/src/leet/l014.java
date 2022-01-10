abaa4072de8669f03d5a436fb74560c9c79a437e
boyLenGit <1024552631@qq.com>
2022-01-10 10:11:32 +0800

    first commit

--- LenLeetCode/src/leet/l014.java
+++ LenLeetCode/src/leet/l014.java
@@ -5,19 +5,24 @@ import java.util.List;
 
 public class l014 {
     public static String longestCommonPrefix(String[] strs) {
-        int public_index = 0;
         String string_result = "";
         int min_length = 0;
+        char compare = ' ';
+        boolean isRepeat;
         for (int i1=0; i1<strs.length; i1++){
             min_length = Math.min(min_length, strs[i1].length());
         }
         for (int i2=0; i2<min_length; i2++){
-            for (int i3=0; i3<strs.length; i3++){
-                if (i3+1<strs.length){
-                    if (strs[i3].toCharArray()[i2]==strs[i3+1].toCharArray()[i2]){
-                        string_result = string_result.concat(String.valueOf(strs[i3].toCharArray()[i2]));
-                    }
+            compare = strs[0].toCharArray()[i2];
+            isRepeat = true;
+            for (int i3=1; i3<strs.length; i3++){
+                if (strs[i3].toCharArray()[i2]!=compare){
+                    isRepeat = false;
+                    break;
                 }
+            }
+            if (isRepeat){
+                string_result = string_result.concat(String.valueOf(strs[0].toCharArray()[i2]));
             }
         }
         return string_result;
