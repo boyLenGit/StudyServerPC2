24b83d16f3eedac37eacbf4d92ec29d50eb5bb1d
boyLenGit <1024552631@qq.com>
2022-01-11 11:08:17 +0800

    first commit

--- LenLeetCode/src/leet/l016.java
+++ LenLeetCode/src/leet/l016.java
@@ -8,8 +8,13 @@ public class l016 {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.parallelSort(nums);
         List<Integer> inputs = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        int sum_min = 0;
         for (int i1=0; i1<inputs.size(); i1++){
-
+            for (int i2=0, i3=inputs.size()-1; i2<i3; ){
+                int sum = inputs.get(i1) + inputs.get(i2) + inputs.get(i3);
+                if (Math.abs(target-sum)==0) return 999;
+                sum_min = Math.abs(target-sum) < Math.abs(target-sum_min) ? sum : sum_min;
+            }
         }
     }
 
