1776d59b9bcd810d3b9785e3a6fa1e99766255d1
boyLenGit <1024552631@qq.com>
2022-01-12 18:44:12 +0800

    first commit

--- LenLeetCode/src/leet/l018.java
+++ LenLeetCode/src/leet/l018.java
@@ -9,12 +9,15 @@ public class l018 {
         Arrays.parallelSort(nums);
         List<List<Integer>> result = new ArrayList<>();
         int sum;
-        for (int i1=0; i1<nums.length; ){
+        for (int i1=0; i1<nums.length; i1++){
             if (nums[i1]>0) break;
             for (int i2=i1+1; i2<nums.length; i2++){
                 for (int i3=i2+1, i4=nums.length-1; i3<i4; ){
                     sum = nums[i1] + nums[i2] + nums[i3] + nums[i4];
-                    if (sum==0) result.add(new ArrayList<>(Arrays.asList(nums[i1], nums[i2], nums[i3], nums[i4])));
+                    if (sum==0) {
+                        result.add(new ArrayList<>(Arrays.asList(nums[i1], nums[i2], nums[i3], nums[i4])));
+                        i3++;
+                    }
                     else if (sum>0) i4--;
                     else i3++;
                 }
