ec6292e985c573a494ee8d4d919aa593424287eb
boyLenGit <1024552631@qq.com>
2022-01-12 23:32:03 +0800

    first commit

--- LenLeetCode/src/leet/l020.java
+++ LenLeetCode/src/leet/l020.java
@@ -3,9 +3,17 @@ package leet;
 public class l020 {
     public static boolean isValid(String s) {
         char[] chars = s.toCharArray();
-        for(int i1=(chars.length/2)-1; i1>=0; i1--){
-            
+        for(int i1=(chars.length/2)-1, i2=(chars.length/2); i1>=0; i1--, i2++){
+            switch (chars[i1]){
+                case '(':
+                    if (chars[i2]!=')') return false;
+                case '{':
+                    if (chars[i2]!='}') return false;
+                case '[':
+                    if (chars[i2]!=']') return false;
+            }
         }
+        return true;
     }
 }
 
