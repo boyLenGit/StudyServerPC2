2bd816fe4945180b367202d5ec142b62a14a58c7
boyLenGit <1024552631@qq.com>
2022-01-13 23:48:33 +0800

    first commit

--- LenLeetCode/src/leet/l021.java
+++ LenLeetCode/src/leet/l021.java
@@ -9,16 +9,21 @@ import java.util.Collections;
 public class l021 {
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ArrayList<Integer> alist1 = new ArrayList<Integer>();
-        do {
-            alist1.add(list1.val);
-            list1 = list1.next;
-        } while (list1 != null);
 
-        do {
+        while (true){
+            alist1.add(list1.val);
+            if (list2.next==null) break;
+            list1 = list1.next;
+        }
+
+        while (true){
             alist1.add(list2.val);
+            if (list2.next==null) break;
             list2 = list2.next;
-        } while (list2 != null);
+        }
+
         Collections.sort(alist1);
+        Collections.reverse(alist1);
         ListNode result = new ListNode();
         for (int i1=0; i1<alist1.size(); i1++){
             if (i1==0){
@@ -33,6 +38,7 @@ public class l021 {
     public static void main(String[] args){
         ListNode input1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode input2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode input3 = new ListNode();
         ListNode result = mergeTwoLists(input1, input2);
     }
 }
