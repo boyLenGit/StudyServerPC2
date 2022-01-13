d526a67bf065e279fc116b2eb5f893c9504a93c3
boyLenGit <1024552631@qq.com>
2022-01-13 17:34:10 +0800

    first commit

--- LenLeetCode/src/leet/l020.java
+++ LenLeetCode/src/leet/l020.java
@@ -5,40 +5,40 @@ public class l020 {
         if (s.length()%2!=0) return false;
         char[] chars = s.toCharArray();
         int isTrue = 1;
+        // "{[]}"情况
+        if ((chars[0]=='{' & chars[chars.length-1]=='}') | (chars[0]=='[' & chars[chars.length-1]==']') | (chars[0]=='(' & chars[chars.length-1]==')')){
+            for(int i1=(chars.length/2), i2=i1+1; i1>=0 & i2<chars.length; i1--, i2++){
+                switch (chars[i1]){
+                    case '(':
+                        if (chars[i2]!=')') isTrue = 0;
+                        continue;
+                    case '{':
+                        if (chars[i2]!='}') isTrue = 0;
+                        continue;
+                    case '[':
+                        if (chars[i2]!=']') isTrue = 0;
+                }
+            }
+            return isTrue == 1;
+        }
         // {}[]()情况
         for(int i1=0, i2=i1+1; i1<chars.length & i2<chars.length; i1++, i2++){
-            switch (chars[i1]){
-                case '(':
-                    if (chars[i2]!=')') isTrue = 0;
-                    continue;
-                case '{':
-                    if (chars[i2]!='}') isTrue = 0;
-                    continue;
-                case '[':
-                    if (chars[i2]!=']') isTrue = 0;
+            if (chars[i1]=='(' & chars[i2]!=')'){
+                isTrue = 0;
+            }else if (chars[i1]=='[' & chars[i2]!=']'){
+                isTrue = 0;
+            }else if (chars[i1]=='{' & chars[i2]!='}'){
+                isTrue = 0;
             }
         }
-
-        for(int i1=(chars.length/2), i2=i1+1; i1>=0 & i2<chars.length; i1--, i2++){
-            switch (chars[i1]){
-                case '(':
-                    if (chars[i2]!=')') isTrue = 0;
-                    continue;
-                case '{':
-                    if (chars[i2]!='}') isTrue = 0;
-                    continue;
-                case '[':
-                    if (chars[i2]!=']') isTrue = 0;
-            }
-        }
-
         return isTrue == 1;
     }
 
     public static void main(String[] args){
-        System.out.println(isValid("("));
+        System.out.println(isValid("){"));
     }
 }
 
 // {}{}
 // 0123
+// "){"
