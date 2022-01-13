28e4e5306cdc4a041658c242b57daddb3f92fbe8
boyLenGit <1024552631@qq.com>
2022-01-13 17:13:20 +0800

    first commit

--- LenLeetCode/src/leet/l020.java
+++ LenLeetCode/src/leet/l020.java
@@ -2,18 +2,41 @@ package leet;
 
 public class l020 {
     public static boolean isValid(String s) {
+        if (s.length()%2!=0) return false;
         char[] chars = s.toCharArray();
-        for(int i1=(chars.length/2)-1, i2=(chars.length/2); i1>=0; i1--, i2++){
+        int isTrue = 1;
+        // {}[]()情况
+        for(int i1=0, i2=i1+1; i1<chars.length & i2<chars.length; i1++, i2++){
             switch (chars[i1]){
                 case '(':
-                    if (chars[i2]!=')') return false;
+                    if (chars[i2]!=')') isTrue = 0;
+                    continue;
                 case '{':
-                    if (chars[i2]!='}') return false;
+                    if (chars[i2]!='}') isTrue = 0;
+                    continue;
                 case '[':
-                    if (chars[i2]!=']') return false;
+                    if (chars[i2]!=']') isTrue = 0;
             }
         }
-        return true;
+
+        for(int i1=(chars.length/2), i2=i1+1; i1>=0 & i2<chars.length; i1--, i2++){
+            switch (chars[i1]){
+                case '(':
+                    if (chars[i2]!=')') isTrue = 0;
+                    continue;
+                case '{':
+                    if (chars[i2]!='}') isTrue = 0;
+                    continue;
+                case '[':
+                    if (chars[i2]!=']') isTrue = 0;
+            }
+        }
+
+        return isTrue == 1;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isValid("("));
     }
 }
 
