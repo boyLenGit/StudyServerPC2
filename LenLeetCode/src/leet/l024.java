098628789877b1eac46271074179c88245fb29e6
boyLenGit <1024552631@qq.com>
2022-01-14 23:26:51 +0800

    first commit

--- LenLeetCode/src/leet/l024.java
+++ LenLeetCode/src/leet/l024.java
@@ -11,5 +11,18 @@ public class l024 {
             if (i1!=0) head = head.next;
             inputs.add(head.val);
         }
+        // 开始交换
+        ArrayList<Integer> result_list = new ArrayList<Integer>();
+        for (int i1=0; i1<inputs.size(); i1=i1+2){
+            if (i1+1<inputs.size()){
+                result_list.add(inputs.get(i1+1));
+                result_list.add(inputs.get(i1));
+                continue;
+            }
+            result_list.add(inputs.get(i1));
+        }
+        // 
     }
 }
+// 12345
+// 21435
