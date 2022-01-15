a5dabe98b00986ba703791192c1cab2c6c86d109
boyLenGit <1024552631@qq.com>
2022-01-15 10:48:01 +0800

    first commit

--- LenLeetCode/src/leet/l024.java
+++ LenLeetCode/src/leet/l024.java
@@ -3,12 +3,15 @@ package leet;
 import leet.help.ListNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class l024 {
     public static ListNode swapPairs(ListNode head) {
         ArrayList<Integer> inputs = new ArrayList<Integer>();
         for (int i1=0; ; i1++){
             if (i1!=0) head = head.next;
+            if (head==null) break;
             inputs.add(head.val);
         }
         // 开始交换
@@ -21,7 +24,19 @@ public class l024 {
             }
             result_list.add(inputs.get(i1));
         }
-        // 
+        // 生成ListNode
+        Collections.reverse(result_list);
+        ListNode result = null;
+        for (int i1=0; i1<result_list.size(); i1++){
+            result = new ListNode(result_list.get(i1), result);
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args){
+        ListNode input1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode()))));
+        ListNode result = swapPairs(input1);
     }
 }
 // 12345
