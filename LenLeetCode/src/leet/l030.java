0f57883f970629cc1202a3b5b0718341c292c1ac
boyLenGit <1024552631@qq.com>
2022-01-16 12:25:50 +0800

    first commit

--- LenLeetCode/src/leet/l030.java
+++ LenLeetCode/src/leet/l030.java
@@ -1,18 +1,24 @@
 package leet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class l030 {
     public static List<Integer> findSubstring(String s, String[] words) {
+        // 提取出单词组成一个list
         ArrayList<String> list_in = new ArrayList<String>();
         int len_word = words[0].length();
-        int start=0;
-        int end=0;
         for (int i1=0; i1<s.length()/words[0].length(); i1++){
-            start = i1*len_word;
-            end = (i1+1)*len_word+1;
             list_in.add(i1, s.substring(i1*len_word, (i1+1)*len_word));
+        }
+        //
+        ArrayList<String> compares = new ArrayList<String>(Arrays.asList(words));
+        for (int i1=0; i1<list_in.size(); i1++){
+            if (compares.contains(list_in.get(i1))){
+                
+            }
         }
         return new ArrayList<Integer>();
     }
