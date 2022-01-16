9d4202858b653acacdc782e0e8af226575abd6b5
boyLenGit <1024552631@qq.com>
2022-01-16 19:07:57 +0800

    first commit

--- LenLeetCode/src/leet/l030.java
+++ LenLeetCode/src/leet/l030.java
@@ -1,5 +1,6 @@
 package leet;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,11 +14,25 @@ public class l030 {
         for (int i1=0; i1<s.length()/words[0].length(); i1++){
             list_in.add(i1, s.substring(i1*len_word, (i1+1)*len_word));
         }
-        //
+        //计算有没有符合要求的
+        ArrayList<Integer> result = new ArrayList<Integer>();
         ArrayList<String> compares = new ArrayList<String>(Arrays.asList(words));
         for (int i1=0; i1<list_in.size(); i1++){
+            for (int i2=0; i2<words.length; i2++){
+                if (compares.contains(list_in.get(i1))){
+                    compares.remove(list_in.get(i1));
+                }
+            }
+
             if (compares.contains(list_in.get(i1))){
-                
+                compares.remove(list_in.get(i1));
+                result.add(i1*len_word);
+                if (compares.size()==0){
+                    return result;
+                }
+            }else {
+                compares = new ArrayList<String>(Arrays.asList(words));
+                result = new ArrayList<Integer>();
             }
         }
         return new ArrayList<Integer>();
