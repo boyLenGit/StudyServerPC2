054a216eb14d563f86e538905620045874599fe4
boyLenGit <1024552631@qq.com>
2022-01-16 22:21:59 +0800

    first commit

--- LenLeetCode/src/leet/l030.java
+++ LenLeetCode/src/leet/l030.java
@@ -7,40 +7,20 @@ import java.util.Collections;
 import java.util.List;
 
 public class l030 {
-    public static List<Integer> findSubstring(String s, String[] words) {
-        // 提取出单词组成一个list
-        ArrayList<String> list_in = new ArrayList<String>();
-        int len_word = words[0].length();
-        for (int i1=0; i1<s.length()/words[0].length(); i1++){
-            list_in.add(i1, s.substring(i1*len_word, (i1+1)*len_word));
-        }
-        //计算有没有符合要求的
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        ArrayList<String> compares = new ArrayList<String>(Arrays.asList(words));
-        for (int i1=0; i1<=list_in.size()-words.length; i1++){
-            compares = new ArrayList<String>(Arrays.asList(words));
-            if (compares.contains(list_in.get(i1))){
-                for (int i2=i1; i2<i1+words.length; i2++){
-                    if (compares.contains(list_in.get(i2))){
-                        compares.remove(compares.indexOf(list_in.get(i2)));
-                        if (compares.size()==0){
-                            result.add(i1*len_word);
-                            break;
-                        }
-                    }else break;
-                }
-            }
-        }
-        return result;
-    }
 
     public static List<Integer> findSubstring2(String s, String[] words) {
         String sub = "";
+        int cnt = 0;
         ArrayList<Integer> result = new ArrayList<Integer>();
         ArrayList<String> compares = new ArrayList<String>(Arrays.asList(words));
-        for (int i1=0; i1+words[0].length()<s.length(); i1++){
+        for (int i1=0; i1+words[0].length()<=s.length(); i1++){
+            compares = new ArrayList<String>(Arrays.asList(words));
             sub = s.substring(i1, i1+words[0].length());
-            if (compares.contains(sub) & (i1+words.length*words[0].length())<s.length()){
+            int debug1 = i1+words.length*words[0].length();
+            int debug2 = s.length();
+            int debug3 = i1+words[0].length();
+            if (compares.contains(sub) & (i1+words.length*words[0].length())<=s.length()){
+                cnt = i1;
                 for (int i2=0; i2<words.length; i2++){
                     if (compares.contains(sub)){
                         compares.remove(compares.indexOf(sub));
@@ -48,8 +28,8 @@ public class l030 {
                             result.add(i1);
                             break;
                         }
-                        i1 = i1+words[0].length();
-                        sub = s.substring(i1, i1+words[0].length()+1);
+                        cnt = cnt+words[0].length();
+                        sub = s.substring(cnt, cnt+words[0].length());
                     }else break;
                 }
             }
@@ -59,10 +39,14 @@ public class l030 {
 
 
     public static void main(String[] args){
-        System.out.println(findSubstring2("lingmindraboofooowingdingbarrwingmonkeypoundcake", new String[]{"fooo","barr","wing","ding","wing"}));
+        System.out.println(findSubstring2("a", new String[]{"a"}));
     }
 }
 //"wordgoodgoodgoodbestword"
 //["word","good","best","good"]
 // "lingmindraboofooowingdingbarrwingmonkeypoundcake"
 //["fooo","barr","wing","ding","wing"]
+// "wordgoodgoodgoodbestword"
+//["word","good","best","good"]
+// "a"
+//["a"]
