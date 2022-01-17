1afcc3950992eb958a264cf1cdc9fa05c234e820
boyLenGit <1024552631@qq.com>
2022-01-17 17:24:28 +0800

    first commit

--- LenLeetCode/src/leet/l704.java
+++ LenLeetCode/src/leet/l704.java
@@ -18,8 +18,31 @@ public class l704 {
         return -1;
     }
 
+    public static int search2_2fenSearch(int[] nums, int target) {
+        for (int i1=nums.length/2;;){
+            if (nums[i1]==target) return i1;
+            else if (nums[i1]>target) {
+                if (i1!=0) if (nums[i1-1]<target) break;
+                i1 = i1/2;
+            }
+            else if (nums[i1]<target) {
+                if (i1!=nums.length-1) if (nums[i1+1]>target) break;
+                i1 = i1 + ((nums.length-i1)/2);
+            }
+            // 放在最底部，用于防止index溢出
+            if (i1<0 | i1>=nums.length) break;
+        }
+        return -1;
+    }
+
     public static void main(String[] args){
-        System.out.println(search(new int[]{}, -3));
+        System.out.println(search2_2fenSearch(new int[]{5}, -3));
+        System.out.println(9/2);
     }
 
 }
+// 123456789    len=9
+// 012345678
+// 12345678     len=8
+// 01234567
+// 9/2=4
