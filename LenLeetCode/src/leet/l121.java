6f90155f2d150e45f7909e0020ff4be6ee7f029a
boyLenGit <1024552631@qq.com>
2022-01-18 22:33:27 +0800

    first commit

--- LenLeetCode/src/leet/l121.java
+++ LenLeetCode/src/leet/l121.java
@@ -10,27 +10,19 @@ public class l121 {
         ArrayList<Integer> inputs = (ArrayList<Integer>) Arrays.stream(prices).boxed().collect(Collectors.toList());
         int[] copyer = prices.clone();
         Arrays.sort(copyer);
-        int single_lock = 0;
         // i1是最小值索引，i2是最大值索引
-        for (int i1=0, i2=copyer.length-1; i1<i2;){
-            int debug1 = inputs.indexOf(copyer[i1]);
-            int debug2 = inputs.indexOf(copyer[i2]);
-            if (inputs.indexOf(copyer[i1])<inputs.indexOf(copyer[i2])){
-                result = Math.max(result, (copyer[i2]-copyer[i1]));
-            }else {
-                if (single_lock==0){
-                    i1++;
-                    single_lock = 1;
-                }else {
-                    i2--;
-                    single_lock = 0;
-                }
+        for (int i1=0; i1<copyer.length; i1++){
+            for (int i2=copyer.length-1; i2>i1; i2--){
+                int debug1 = inputs.indexOf(copyer[i1]);
+                int debug2 = inputs.indexOf(copyer[i2]);
+                if (inputs.indexOf(copyer[i1])<inputs.indexOf(copyer[i2])) return copyer[i2]-copyer[i1];
             }
         }
         return result;
     }
 
     public static void main(String[] as){
-        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{2,1,2,0,1}));
     }
 }
+// [2,1,2,0,1]
