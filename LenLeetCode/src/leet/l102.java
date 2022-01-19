30a69a75c8bec18446c66e4289682856a443b11b
boyLenGit <1024552631@qq.com>
2022-01-19 22:32:29 +0800

    first commit

--- LenLeetCode/src/leet/l102.java
+++ LenLeetCode/src/leet/l102.java
@@ -13,13 +13,21 @@ public class l102 {
         ArrayList<TreeNode> inputs = new ArrayList<>();
         ArrayList<TreeNode> temp_inputs = new ArrayList<>();
         inputs.add(root);
-        for (int i1=0; ; ){
+        int cntEnd;
+        // i1没有任何意义，仅记录循环次数
+        while (true){
+            // 初始化
+            cntEnd = 0;
+            // 提取当前[TreeNode]的val
             temp_result = new ArrayList<>();
             for (int i2=0; i2<inputs.size(); i2++){
                 temp_result.add(inputs.get(i2).val);
+                if (inputs.get(i2).right==null & inputs.get(i2).left==null) cntEnd++;
             }
             result.add(temp_result);
-            if (inputs.get(i1).left==null & inputs.get(i1).right==null) break;
+            // 判定结束条件
+            if (cntEnd==inputs.size()) break;
+            // 生成下一组将要遍历的[TreeNode]
             temp_inputs = new ArrayList<>();
             for (int i3=0; i3<inputs.size(); i3++){
                 if (inputs.get(i3).left!=null) temp_inputs.add(inputs.get(i3).left);
