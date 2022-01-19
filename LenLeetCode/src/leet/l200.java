7f38eb42eff86ed63571f6feda21463e00bd4c29
boyLenGit <1024552631@qq.com>
2022-01-19 10:25:29 +0800

    first commit

--- LenLeetCode/src/leet/l200.java
+++ LenLeetCode/src/leet/l200.java
@@ -15,34 +15,74 @@ public class l200 {
         for (int i1=0; i1<grid.length; i1++){
             for (int i2=0; i2<grid[0].length; i2++){
                 if (inputs[i1][i2]==0) continue;
-                // 取值
-                if (i2!=0) {
-                    if (inputs[i1][i2-1]!=0) value=inputs[i1][i2-1];
+//                // 取值
+//                if (i2!=0 & i1!=0){
+//                    if (inputs[i1][i2-1]!=0 & inputs[i1-1][i2]==0){
+//                        value = inputs[i1][i2-1];
+//                    }else if (inputs[i1][i2-1]==0 & inputs[i1-1][i2]!=0){
+//                        value = inputs[i1-1][i2];
+//                    }else if (inputs[i1][i2-1]!=0 & inputs[i1-1][i2]!=0){
+//                        value = inputs[i1][i2-1];
+//                    }else if (inputs[i1][i2-1]==0 & inputs[i1-1][i2]==0){
+//                        value++;
+//                        inputs[i1][i2] = value;
+//                    }
+//                }else if (i2!=0 & i1==0){
+//                    if (inputs[i1][i2-1]==0){
+//                        value++;
+//                        inputs[i1][i2] = value;
+//                    }
+//                }else if (i1!=0 & i2==0){
+//                    if (inputs[i1-1][i2]==0){
+//                        value++;
+//                        inputs[i1][i2] = value;
+//                    }
+//                }else if (i1==0 & i2==0){
+//                    value++;
+//                    inputs[i1][i2] = value;
+//                }
+//
+//                // 赋值
+//                if (i2!=grid[0].length-1){
+//                    if (inputs[i1][i2+1]!=0) inputs[i1][i2+1]=value;
+//                }
+//                if (i1!=grid.length-1){
+//                    if (inputs[i1+1][i2]!=0) inputs[i1+1][i2]=value;
+//                }
+                // 上
+                for (int i3=i1-1; i3>=0; i3--){
+                    if (inputs[i3][i2]==1);
+
                 }
-                else if (i1!=0) {
-                    if (inputs[i1-1][i2]!=0) value=inputs[i1-1][i2];
-                }else if (i1==0 & i2==0){
-                    value++;
-                }
-                // 赋值
-                if (i2!=grid[0].length-1){
-                    if (inputs[i1][i2+1]!=0) inputs[i1][i2+1]=value;
-                }
-                if (i1!=grid.length-1){
-                    if (inputs[i1+1][i2]!=0) inputs[i1+1][i2]=value;
-                }
+                // 下
+                // 左
+                // 右
             }
         }
         LenLog.printInt2vArray(inputs);
         return 0;
     }
 
+    public static int numIslands_dg(char[][] grid) {
+        int[][] inputs = new int[grid.length][grid[0].length];
+        for (int i1=0; i1<grid.length; i1++){
+            for (int i2=0; i2<grid[0].length; i2++){
+                inputs[i1][i2] = grid[i1][i2]-'0';
+            }
+
+        }
+    }
+
+    public static void dg(){
+
+    }
+
     public static void main(String[] args){
         char[][] input = new char[][]{
+                {'1','1','0','1','0'},
                 {'1','1','1','1','0'},
                 {'1','1','0','1','0'},
-                {'1','1','0','0','0'},
-                {'0','0','0','0','1'}};
+                {'0','0','1','0','1'}};
         numIslands(input);
     }
 }
