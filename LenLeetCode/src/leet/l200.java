e44d5e4d44890fbc7b2b8dfadc85634e63db4781
boyLenGit <1024552631@qq.com>
2022-01-19 10:39:24 +0800

    first commit

--- LenLeetCode/src/leet/l200.java
+++ LenLeetCode/src/leet/l200.java
@@ -65,16 +65,51 @@ public class l200 {
 
     public static int numIslands_dg(char[][] grid) {
         int[][] inputs = new int[grid.length][grid[0].length];
+        int value = 2;
         for (int i1=0; i1<grid.length; i1++){
             for (int i2=0; i2<grid[0].length; i2++){
                 inputs[i1][i2] = grid[i1][i2]-'0';
             }
-
         }
+        for (int i1=0; i1<grid.length; i1++){
+            for (int i2=0; i2<grid[0].length; i2++){
+                if (inputs[i1][i2]==0) continue;
+                else if (inputs[i1][i2]==1) {
+                    value++;
+                    inputs[i1][i2] = value;
+                }
+                dg(inputs, i1, i2);
+            }
+        }
+        LenLog.printInt2vArray(inputs);
+        return 0;
     }
 
-    public static void dg(){
-
+    public static void dg(int[][] inputs, int i1, int i2){
+        if (i1-1>=0){
+            if (inputs[i1-1][i2]==1){
+                inputs[i1-1][i2] = inputs[i1][i2];
+                dg(inputs, i1-1, i2);
+            }
+        }
+        if (i1+1<inputs.length){
+            if (inputs[i1+1][i2]==1){
+                inputs[i1+1][i2] = inputs[i1][i2];
+                dg(inputs, i1+1, i2);
+            }
+        }
+        if (i2-1>=0){
+            if (inputs[i1][i2-1]==1){
+                inputs[i1][i2-1] = inputs[i1][i2-1];
+                dg(inputs, i1, i2-1);
+            }
+        }
+        if (i2+1<inputs[0].length){
+            if (inputs[i1][i2+1]==1){
+                inputs[i1][i2+1] = inputs[i1][i2+1];
+                dg(inputs, i1, i2+1);
+            }
+        }
     }
 
     public static void main(String[] args){
@@ -83,6 +118,6 @@ public class l200 {
                 {'1','1','1','1','0'},
                 {'1','1','0','1','0'},
                 {'0','0','1','0','1'}};
-        numIslands(input);
+        numIslands_dg(input);
     }
 }
