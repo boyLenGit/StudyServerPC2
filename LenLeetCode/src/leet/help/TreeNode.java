259fd2b9dbe190a1ec02f8712276b4ff2c1505c8
boyLenGit <1024552631@qq.com>
2022-01-19 11:22:06 +0800

    first commit

---
+++ LenLeetCode/src/leet/help/TreeNode.java
@@ -0,0 +1,17 @@
+package leet.help;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) { this.val = val; }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+      }
+}
---
+++ LenLeetCode/src/leet/l102.java
@@ -0,0 +1,17 @@
+package leet;
+
+import leet.help.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class l102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> temp_result = new ArrayList<>();
+        ArrayList<TreeNode> items = new ArrayList<>();
+        for (int i1=0; ; ){
+
+        }
+    }
+}
