0418d16b6708fccba2b9d73390eadd0fc62ca3b3
boyLenGit <1024552631@qq.com>
2022-01-20 15:31:15 +0800

    first commit

--- LenLeetCode/src/leet/l611.java
+++ LenLeetCode/src/leet/l611.java
@@ -9,7 +9,7 @@ public class l611 {
         for (int i1=0; i1<nums.length-2; i1++){
             for (int i2=i1+1; i2<nums.length-1; i2++){
                 for (int i3=i2+1; i3<nums.length; i3++){
-                    if (nums[i1]+nums[i1+1]>nums[i1+2]) cnt++;
+                    if (nums[i1]+nums[i2]>nums[i3]) cnt++;
                     else break;
                 }
             }
