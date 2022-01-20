3337509ed6a3e340773b6aed2cc8e4fc9f9f4c20
boyLenGit <1024552631@qq.com>
2022-01-20 15:29:48 +0800

    first commit

--- LenLeetCode/src/leet/l611.java
+++ LenLeetCode/src/leet/l611.java
@@ -1,7 +1,24 @@
 package leet;
 
+import java.util.Arrays;
+
 public class l611 {
     public static int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int cnt = 0;
+        for (int i1=0; i1<nums.length-2; i1++){
+            for (int i2=i1+1; i2<nums.length-1; i2++){
+                for (int i3=i2+1; i3<nums.length; i3++){
+                    if (nums[i1]+nums[i1+1]>nums[i1+2]) cnt++;
+                    else break;
+                }
+            }
 
+        }
+        return cnt;
+    }
+
+    public static void main(String[] a){
+        System.out.println(triangleNumber(new int[]{2,2,3,4}));
     }
 }
