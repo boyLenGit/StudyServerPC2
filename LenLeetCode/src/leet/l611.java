d7a59d794b26012b80f418c2f6443618a284f35d
boyLenGit <1024552631@qq.com>
2022-01-20 15:36:56 +0800

    first commit

--- LenLeetCode/src/leet/l611.java
+++ LenLeetCode/src/leet/l611.java
@@ -18,7 +18,30 @@ public class l611 {
         return cnt;
     }
 
+    public static int triangleNumber2(int[] nums) {
+        // 双指针法优化速度
+        Arrays.sort(nums);
+        int cnt = 0;
+        for (int i1=0; i1<nums.length-2; i1++){
+            for (int i2=i1+1, i3=i1+2; i2<nums.length-1; ){
+                if (i3>=nums.length) {
+                    i2++;
+                    i3 = i2 + 1;
+                }
+                if (nums[i1]+nums[i2]>nums[i3]) {
+                    cnt++;
+                    i3++;
+                }else {
+                    i2++;
+                    i3 = i2 + 1;
+                }
+            }
+
+        }
+        return cnt;
+    }
+
     public static void main(String[] a){
-        System.out.println(triangleNumber(new int[]{2,2,3,4}));
+        System.out.println(triangleNumber2(new int[]{2,2,3,4}));
     }
 }
