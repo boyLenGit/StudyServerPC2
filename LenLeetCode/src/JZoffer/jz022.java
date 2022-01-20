3e7688e0a28c4b75bb2c808b06b7298c00598ea1
boyLenGit <1024552631@qq.com>
2022-01-20 23:49:59 +0800

    first commit

--- LenLeetCode/src/JZoffer/jz022.java
+++ LenLeetCode/src/JZoffer/jz022.java
@@ -7,9 +7,7 @@ import java.util.ArrayList;
 
 public class jz022 {
     public static ListNode getKthFromEnd(ListNode head, int k) {
-        ListNode result = null;
         ArrayList<ListNode> nodes = new ArrayList<>();
-        ArrayList<Integer> list_in = new ArrayList<>();
         for (int i1=0; ; i1++){
             if (i1!=0) head = head.next;
             if (head==null) break;
@@ -19,7 +17,7 @@ public class jz022 {
     }
 
     public static void main(String[] s){
-        ListNode inputs = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode())))));
+        ListNode inputs = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         ListNode result = getKthFromEnd(inputs, 2);
         System.out.println(result.toString());
     }
---
+++ LenLeetCode/src/leet/l046.java
@@ -0,0 +1,37 @@
+package leet;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class l046 {
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int num: nums){
+            result.add(new ArrayList<>(Collections.singleton(num)));
+        }
+        dg(nums, result);
+        return result;
+    }
+
+    public static void dg(int[] ilist, List<List<Integer>> result){
+        List<List<Integer>> temp = new ArrayList<>();
+        for (List<Integer> lists : result){
+             for (Integer integer : ilist){
+                 if (!lists.contains(integer)) {  // 如果不包含该字符，则add上
+                     lists.add(integer);
+                     temp.add(lists.subList(0, lists.size()));
+                     lists.remove(lists.size()-1);
+                 }
+             }
+        }
+        result = temp;
+        if (result.get(0).size()!=ilist.length) dg(ilist, result);
+    }
+
+    public static void main(String[] a){
+        List<List<Integer>> result = permute(new int[]{1,2,3});
+        System.out.println(Arrays.toString(result.toArray()));
+    }
+}
