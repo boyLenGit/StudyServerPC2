cdad6d4ec465eb7dfda83bdb63179b42620c59fb
boyLenGit <1024552631@qq.com>
2022-01-21 23:09:11 +0800

    first commit

--- LenLeetCode/src/leet/l128.java
+++ LenLeetCode/src/leet/l128.java
@@ -7,18 +7,19 @@ public class l128 {
         if (nums.length==0) return 0;
         else if (nums.length==1) return 1;
         Arrays.sort(nums);
-        int len = 0;
-        int res = 0;
+        int len = 1;
+        int res = 1;
         for (int i1=1; i1<nums.length; i1++){
-            if (nums[i1]==nums[i1-1]) len++;
-            else len = 0;
+            if (nums[i1]==nums[i1-1]) continue;
+            if (nums[i1]==nums[i1-1]+1) len++;
+            else len = 1;
             res = Math.max(res, len);
         }
-        return len;
+        return res;
     }
 
     public static void main(String[] a){
-        int[] inputs = new int[]{100,4,200,1,3,2};
+        int[] inputs = new int[]{1,2,0,1};
         longestConsecutive(inputs);
     }
 }
