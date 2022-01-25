50b7d38c1ffa3fa0cb275d77f5fefd3e3cf5a6bb
boyLenGit <1024552631@qq.com>
2022-01-25 15:35:34 +0800

    first commit

--- LenLeetCode/src/leet/l152.java
+++ LenLeetCode/src/leet/l152.java
@@ -5,30 +5,24 @@ public class l152 {
         if (nums.length==0) return 0;
         int res = nums[0];
         int res_temp = nums[0];
-        int res_temp_negative = 1;
-        int cnt_negative = 0;
+        int res_temp_negative = nums[0];
         for (int i1=1; i1<nums.length; i1++){
-            if (nums[i1]==0){
-                res = Math.max(res, nums[i1]);
-                if (i1+1<nums.length) {
-                    res_temp = nums[i1+1];
-                    i1++;
-                }
-                else return res;
-                continue;
-            }else if (nums[i1-1]<0){
-                res_temp_negative = 1;
+            if (nums[i1-1]<0){
+                res_temp_negative = nums[i1];
             }
-            res_temp *= nums[i1];
-            res_temp_negative *= nums[i1];
-            res = Math.max(Math.max(res, res_temp), res_temp_negative);
+            res_temp = nums[i1-1]==0 ? nums[i1] : nums[i1]*res_temp;
+            res_temp_negative = nums[i1-1]<0 ? res_temp_negative : res_temp_negative*nums[i1];
+            res = Math.max(Math.max(res, nums[i1]), Math.max(res_temp_negative, res_temp));
         }
         return res;
     }
 
     public static void main(String[] sa){
-        int[] inputs = new int[]{-3,-1,-1};
+        int[] inputs = new int[]{2,3,-2,4};
         System.out.println(maxProduct(inputs));
     }
 }
 // [-3,-1,-1]
+// [-5,0,2]
+// [2,3,-2,4]
+//[-2,0,-1]
