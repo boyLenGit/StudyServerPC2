57e1f2559d2cc51c5953c6588ab4e19debd47f8c
boyLenGit <1024552631@qq.com>
2022-01-26 18:12:42 +0800

    first commit

--- LenLeetCode/src/leet/l912.java
+++ LenLeetCode/src/leet/l912.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 public class l912 {
     public static int[] sortArray(int[] nums) {
+        // 无法通过,超时
         int min;
         for (int i1=0; i1<nums.length; i1++){
             min = i1;
@@ -19,8 +20,29 @@ public class l912 {
     }
 
     public static int[] sortArray2(int[] nums) {
-        lenQS(nums, 0, nums.length-1);
+        dg(nums, 0, nums.length-1);
         return nums;
+    }
+
+    public static void dg(int[] inputs, int start, int end){
+        if (start>end)return;
+        int exchange;
+        int i_start = start;
+        int i_end = end;
+        int compare = inputs[start];
+        while (i_start<i_end){
+            while (inputs[i_end]>=compare & i_start<i_end) i_end--;
+            while (inputs[i_start]<=compare & i_start<i_end) i_start++;
+            if (i_start<i_end){
+                exchange = inputs[i_start];
+                inputs[i_start] = inputs[i_end];
+                inputs[i_end] = exchange;
+            }
+        }
+        inputs[start] = inputs[i_start];
+        inputs[i_start] = compare;
+        dg(inputs, start, i_end-1);
+        dg(inputs, i_end+1, end);
     }
 
     public static void quickSort(int[] inputs, int start, int end){
@@ -60,8 +82,8 @@ public class l912 {
         int temp = inputs[start];
 
         while (i_start<i_end){
-            while (inputs[i_end]>=temp) i_end--;
-            while (inputs[i_start]<=temp) i_start++;
+            while (inputs[i_end]>=temp & i_start<i_end) i_end--;
+            while (inputs[i_start]<=temp & i_start<i_end) i_start++;
             if (i_start<i_end){
                 exchange = inputs[i_end];
                 inputs[i_end] = inputs[i_start];
