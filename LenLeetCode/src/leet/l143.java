135ff2d2e86a1a9c051e398778e2b886a0b23c19
boyLenGit <1024552631@qq.com>
2022-01-27 11:19:43 +0800

    first commit

--- LenLeetCode/src/leet/l143.java
+++ LenLeetCode/src/leet/l143.java
@@ -8,26 +8,30 @@ import java.util.ArrayList;
 public class l143 {
     public static void reorderList(ListNode head) {
         ArrayList<Integer> inputs = new ArrayList<>();
+        ListNode temp_listNode = head;
         for (int i1=0; ;i1++){
-            if (i1!=0) head = head.next;
-            if (head==null)break;
-            inputs.add(head.val);
+            if (i1!=0) temp_listNode = temp_listNode.next;
+            if (temp_listNode==null)break;
+            inputs.add(temp_listNode.val);
         }
         ArrayList<Integer> list_res = new ArrayList<>();
         for (int i1=0; i1<(inputs.size()+1)/2; i1++){
             list_res.add(inputs.get(i1));
             list_res.add(inputs.get(inputs.size()-1-i1));
         }
-        head = null;
-        for (int i1=0; i1<list_res.size(); i1++){
-            head = new ListNode(list_res.get(i1), head);
-        }
-        int i1=10;
-        LenLog.printString(String.valueOf(i1));
+        dg(head, list_res);
+    }
+
+    public static void dg(ListNode listNode, ArrayList<Integer> vals){
+        if (listNode==null || vals.size()==0) return;
+        listNode.val = vals.get(0);
+        vals.remove(0);
+        dg(listNode.next, vals);
     }
 
     public static void main(String[] a){
         ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
         reorderList(listNode);
+        System.out.println(listNode.toString());
     }
 }
