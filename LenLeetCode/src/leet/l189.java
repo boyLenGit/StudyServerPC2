c9438a272834b6bc1144f2c05aba70ff9986015a
boyLenGit <1024552631@qq.com>
2022-01-27 16:15:56 +0800

    first commit

--- LenLeetCode/src/leet/l189.java
+++ LenLeetCode/src/leet/l189.java
@@ -2,6 +2,8 @@ package leet;
 
 public class l189 {
     public static void rotate(int[] nums, int k) {
+        while (k>=nums.length) k = k - nums.length;
+        if (nums.length==1) return;
         int[] copy_nums = nums.clone();
         for (int i1=0; i1<nums.length; i1++){
             if (i1+k<nums.length)nums[i1+k] = copy_nums[i1];
@@ -10,7 +12,7 @@ public class l189 {
     }
 
     public static void main(String[] a){
-        int[] inputs = new int[]{1,2,3,4,5,6,7};
+        int[] inputs = new int[]{1,2};
         rotate(inputs, 3);
         System.out.println(inputs);
     }
