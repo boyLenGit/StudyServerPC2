6acac9d5c7b615c28f2663e9163ea7b98c01ad96
boyLenGit <1024552631@qq.com>
2022-01-27 16:27:02 +0800

    first commit

--- LenLeetCode/src/leet/l189.java
+++ LenLeetCode/src/leet/l189.java
@@ -1,5 +1,7 @@
 package leet;
 
+import java.util.Arrays;
+
 public class l189 {
     public static void rotate(int[] nums, int k) {
         while (k>=nums.length) k = k - nums.length;
@@ -11,9 +13,22 @@ public class l189 {
         }
     }
 
+    public static void rotate2(int[] nums, int k) {
+        // 优化空间占用,改为使用k大小的临时数组
+        while (k>=nums.length) k = k - nums.length;
+        if (nums.length==1) return;
+        int[] move_nums = Arrays.copyOfRange(nums, nums.length-k, nums.length);
+        for (int i1=nums.length-1-k; i1>=0; i1--){
+            nums[nums.length-1] = nums[i1];
+        }
+        for (int i1=0; i1<k; i1++){
+            nums[i1] = move_nums[i1];
+        }
+    }
+
     public static void main(String[] a){
-        int[] inputs = new int[]{1,2};
-        rotate(inputs, 3);
+        int[] inputs = new int[]{1,2,3,4,5,6,7};
+        rotate2(inputs, 3);
         System.out.println(inputs);
     }
 }
