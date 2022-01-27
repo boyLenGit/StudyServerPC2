0874c2e9497231ba4a4bcc908d21235cebc690c8
boyLenGit <1024552631@qq.com>
2022-01-27 11:41:56 +0800

    first commit

--- LenLeetCode/src/leet/l143.java
+++ LenLeetCode/src/leet/l143.java
@@ -4,6 +4,7 @@ import leet.help.ListNode;
 import util.LenLog;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class l143 {
     public static void reorderList(ListNode head) {
@@ -29,9 +30,44 @@ public class l143 {
         dg(listNode.next, vals);
     }
 
+    public static void reorderList2(ListNode head) {
+        // 更改算法为:交换
+        ListNode temp_listnode = head;
+        int length = 0;
+        while (temp_listnode!=null) {
+            length++;
+            temp_listnode = temp_listnode.next;
+        }
+        for (int i1=0; i1<(length+1)/2; i1++){
+            i1++;
+            dg_move(head, head, -1, 0, i1);
+        }
+        LenLog.printString("1");
+    }
+
+    public static int dg_move(ListNode full_listNode, ListNode listNode, int last_value, int index, int positon){
+        if (listNode==null){
+            dg_replace(full_listNode, last_value, index, positon);
+            return -1;
+        }
+        int this_value = listNode.val;
+        listNode.val = last_value;
+        last_value = dg_move(full_listNode, listNode.next, this_value, index, positon);
+        return last_value;
+    }
+
+    public static void dg_replace(ListNode listNode, int replace_value, int index, int positon){
+        if (index!=positon){
+            index++;
+            dg_replace(listNode.next, replace_value, index, positon);
+        }
+        listNode.val = replace_value;
+    }
+
     public static void main(String[] a){
         ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
-        reorderList(listNode);
+        ListNode listNode2 = null;
+        reorderList2(listNode);
         System.out.println(listNode.toString());
     }
 }
