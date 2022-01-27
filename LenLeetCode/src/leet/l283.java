49602f2c8c390667fa6da29a7ad3119dd69c9ec2
boyLenGit <1024552631@qq.com>
2022-01-27 14:58:19 +0800

    first commit

--- LenLeetCode/src/leet/l283.java
+++ LenLeetCode/src/leet/l283.java
@@ -1,13 +1,24 @@
 package leet;
 
+import java.util.Arrays;
+
 public class l283 {
     public static void moveZeroes(int[] nums) {
         int count = 0;
-        for (int i1=0; i1<nums.length; i1++){
+        for (int i1=0; i1+count<nums.length; i1++){
             if (nums[i1]==0){
                 count++;
             }
             nums[i1] = nums[count+i1];
         }
+        for (int i1=0; i1<count; i1++){
+            nums[nums.length-1-i1] = 0;
+        }
+    }
+
+    public static void main(String[] a){
+        int[] input1 = new int[]{0,1,0,3,12};
+        moveZeroes(input1);
+        System.out.println(Arrays.toString(input1));
     }
 }
