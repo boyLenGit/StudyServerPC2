069198249f09f3f818b089f311c7fe90c35464da
boyLenGit <1024552631@qq.com>
2022-01-28 17:12:48 +0800

    first commit

--- LenLeetCode/src/leet/l043.java
+++ LenLeetCode/src/leet/l043.java
@@ -5,7 +5,7 @@ public class l043 {
         char[] chars1 = num1.toCharArray();
         char[] chars2 = num2.toCharArray();
         int over = 0;
-        int temp = 0, temp1 = 0, temp2 = 0;
+        int temp, temp1, temp2;
         String right = "";
         for (int i1=num1.length()-1, i2=num2.length()-1; i1>=0 | i2>=0; i1--, i2--){
             temp1 = i1>=0 ? ChartoInt(chars1[i1]) : 1;
@@ -14,6 +14,7 @@ public class l043 {
             over = temp / 10;
             right = String.valueOf(temp%10).concat(right);
         }
+        if (over!=0) right = String.valueOf(over).concat(right);
         return right;
     }
 
@@ -21,4 +22,7 @@ public class l043 {
         return in - '0';
     }
 
+    public static void main(String[] a){
+        System.out.println(multiply("9", "9"));
+    }
 }
