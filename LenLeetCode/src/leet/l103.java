0af7f280a78ea34d975fb1336035200ff6a486c6
boyLenGit <1024552631@qq.com>
2022-01-29 09:36:51 +0800

    first commit

--- LenLeetCode/src/leet/l103.java
+++ LenLeetCode/src/leet/l103.java
@@ -14,22 +14,19 @@ public class l103 {
         List<Integer> res_temp;
         int toRight = 1;
         for (int i1=0; ;){
+            if (inputs.size()==1 & inputs.get(0)==null) break;
             // 从ArrayList中提取内容
             res_temp = new ArrayList<>();
-            for (int i2=0; i2<inputs.size(); i2++){
-                res_temp.add(inputs.get(i2).val);
+            for (int i2=0, i2_reverse=inputs.size()-1; i2<inputs.size(); i2++, i2_reverse--){
+                if (toRight==1) res_temp.add(inputs.get(i2).val);
+                else if (toRight==0) res_temp.add(inputs.get(i2_reverse).val);
             }
             res.add(res_temp);
             // 将TreeNode保存到List中
             inputs_temp = new ArrayList<>();
             for (int i2=0; i2<inputs.size(); i2++){
-                if (toRight==1){
-                    if (inputs.get(i2).left!=null) inputs_temp.add(inputs.get(i2).left);
-                    if (inputs.get(i2).right!=null) inputs_temp.add(inputs.get(i2).right);
-                }else if (toRight==0){
-                    if (inputs.get(i2).right!=null)  inputs_temp.add(inputs.get(i2).right);
-                    if (inputs.get(i2).left!=null)  inputs_temp.add(inputs.get(i2).left);
-                }
+                if (inputs.get(i2).left!=null) inputs_temp.add(inputs.get(i2).left);
+                if (inputs.get(i2).right!=null) inputs_temp.add(inputs.get(i2).right);
             }
             if (inputs_temp.size()==0) break;
             toRight = toRight==1 ? 0 : 1;
@@ -42,6 +39,6 @@ public class l103 {
         TreeNode input1 = new TreeNode(3,
                 new TreeNode(9),
                 new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-        zigzagLevelOrder(null);
+        zigzagLevelOrder(input1);
     }
 }
