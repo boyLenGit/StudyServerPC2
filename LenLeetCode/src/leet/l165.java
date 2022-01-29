b2aee0b02175fe23d494515437ab5b6210d49c28
boyLenGit <1024552631@qq.com>
2022-01-29 10:38:40 +0800

    first commit

--- LenLeetCode/src/leet/l165.java
+++ LenLeetCode/src/leet/l165.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 public class l165 {
     public static int compareVersion(String version1, String version2) {
         String[] in1 = version1.split("\\.");
-        String[] in2 = version1.split("\\.");
-        return 0;
+        String[] in2 = version2.split("\\.");
+        // 先比较左侧
+        if (Integer.parseInt(in1[0])>Integer.parseInt(in2[0])) return 1;
+        else if (Integer.parseInt(in1[0])<Integer.parseInt(in2[0])) return -1;
+        // 在比较左侧
+        for (int i1=0; i1<Math.min(in1[1].length(), in2[1].length()); i1++){
+            if ((int) in1[1].charAt(i1) > (int) in2[1].charAt(i1)) return 1;
+            else if ((int) in1[1].charAt(i1) < (int) in2[1].charAt(i1)) return -1;
+        }
+        if (in1[1].length()==in2[1].length()) return 0;
+        return in1[1].length()>in2[1].length() ? 1 : -1;
     }
 
     public static void main(String[] a){
-        String aa = ".9";
-        System.out.println(Arrays.toString(aa.split("\\.")));
+        compareVersion("1.01", "1.001");
     }
 }
