b554826c1a2254f3e0db8e38dfb529b9d36d735e
boyLenGit <1024552631@qq.com>
2022-01-29 23:49:32 +0800

    first commit

--- LenLeetCode/src/meet/m01_01.java
+++ LenLeetCode/src/meet/m01_01.java
@@ -2,10 +2,7 @@ package meet;
 
 public class m01_01 {
     public static boolean isUnique(String astr) {
-        for (int i1=0; i1<astr.length()-1; i1++){
-            String debug = astr.substring(i1, i1 + 1);
-            if (!astr.contains(astr.substring(i1, i1 + 1))) return false;
-        }
+        for (int i1=0; i1<astr.length()-1; i1++) if (astr.lastIndexOf(String.valueOf(astr.charAt(i1)))!=i1) return false;
         return true;
     }
 
