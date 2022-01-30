935a17164a0d28d545e12b19f151ad31eb55886e
boyLenGit <1024552631@qq.com>
2022-01-30 23:31:23 +0800

    first commit

--- LenLeetCode/src/leet/l560.java
+++ LenLeetCode/src/leet/l560.java
@@ -7,24 +7,25 @@ public class l560 {
         int res = 0;
         int sum;
         for (int i1=0; i1<nums.length; i1++){
-            if (nums[i1]<k){
-                sum = 0;
-                for (int i2=i1+1; i2<nums.length; i2++){
-                    sum += nums[i2];
-                    if (sum>k) break;
-                    else if (sum==k) {
-                        res++;
-                        break;
-                    }
+            sum = nums[i1];
+            for (int i2=i1+1; i2<nums.length; i2++){
+                sum += nums[i2];
+                if (sum>k) break;
+                else if (sum==k) {
+                    res++;
                 }
             }
-
+            if (nums[i1]==k) res++;
         }
         return res;
     }
 
     public static void main(String[] arg){
-        int[] in1 = new int[]{1,1,1};
-        System.out.println(subarraySum(in1, 2));
+        int[] in1 = new int[]{28,54,7,-70,22,65,-6};
+        System.out.println(subarraySum(in1, 100));
     }
 }
+//[1,-1,0]
+//0
+// [28,54,7,-70,22,65,-6]
+//100
