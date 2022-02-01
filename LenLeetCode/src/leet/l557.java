8bf6f5a9b118510855c84e84a41c2216110df099
boyLenGit <1024552631@qq.com>
2022-02-01 19:35:11 +0800

    first commit

---
+++ LenLeetCode/src/leet/l557.java
@@ -0,0 +1,30 @@
+package leet;
+
+import java.util.ArrayList;
+
+public class l557 {
+    public static String reverseWords(String s) {
+        // 除去最前面的空格
+        int cnt_right_null = 0, cnt_left_null = s.length();
+        for (int i1=0; i1<s.length(); i1++) cnt_right_null++;
+        for (int i1=s.length()-1; i1>=0; i1--) cnt_left_null--;
+        s = s.substring(cnt_right_null, cnt_left_null);
+        // 翻转
+        int cnt = 0;
+        StringBuilder res = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        for (int i1=0; i1<s.length(); i1++){
+            if (s.charAt(i1)==' '){  // 启动遍历
+                temp.reverse();
+                res.append(" ").append(temp);
+                temp = new StringBuilder();
+            }
+            temp.append(s.charAt(i1));
+        }
+        return (String) res;
+    }
+
+    public static void main(String[] adr){
+        System.out.println(reverseWords("  "));
+    }
+}
