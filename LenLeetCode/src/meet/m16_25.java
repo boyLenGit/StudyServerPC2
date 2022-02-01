9328df10d990537015dead0c5ac0bd46e8cf7f4c
boyLenGit <1024552631@qq.com>
2022-02-01 16:43:02 +0800

    first commit

--- LenLeetCode/src/meet/m16_25.java
+++ LenLeetCode/src/meet/m16_25.java
@@ -8,15 +8,22 @@ import java.util.HashMap;
 public class m16_25 {
     public static void main(String[] arg){
         LRUCache cache = new LRUCache(2);
-        cache.put(1, 1);
-        cache.put(2, 2);
-        System.out.println(cache.get(1));       // 返回  1
-        cache.put(3, 3);                        // 该操作会使得密钥 2 作废
-        System.out.println(cache.get(2));       // 返回 -1 (未找到)
-        cache.put(4, 4);                        // 该操作会使得密钥 1 作废
-        System.out.println(cache.get(1));       // 返回 -1 (未找到)
-        System.out.println(cache.get(3));       // 返回  3
-        System.out.println(cache.get(4));       // 返回  4
+//        cache.put(1, 1);
+//        cache.put(2, 2);
+//        System.out.println(cache.get(1));       // 返回  1
+//        cache.put(3, 3);                        // 该操作会使得密钥 2 作废
+//        System.out.println(cache.get(2));       // 返回 -1 (未找到)
+//        cache.put(4, 4);                        // 该操作会使得密钥 1 作废
+//        System.out.println(cache.get(1));       // 返回 -1 (未找到)
+//        System.out.println(cache.get(3));       // 返回  3
+//        System.out.println(cache.get(4));       // 返回  4
+        System.out.println(cache.get(2));
+        cache.put(2, 6);
+        System.out.println(cache.get(1));
+        cache.put(1, 5);
+        cache.put(1, 2);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(2));
     }
 }
 
@@ -24,7 +31,6 @@ class LRUCache {
     private HashMap<Integer, Integer> map = new HashMap<>();
     private int capacity;
     private int cnt = 0;
-    private int cnt_grow = 0;
     private int[] stack;
 
     public LRUCache(int capacity) {
@@ -37,7 +43,7 @@ class LRUCache {
         if (!map.containsKey(key)) return -1;
         // 存stack
         stack[cnt] = key;
-        cnt = cnt==this.capacity ? cnt-this.capacity : cnt+1;
+        cnt = cnt==this.capacity-1 ? cnt-(this.capacity-1) : cnt+1;
         // 读map
         return map.get(key);
     }
@@ -46,13 +52,13 @@ class LRUCache {
         if (map.size()==this.capacity){
             cnt = cnt>=capacity ? 0 : cnt;
             map.remove(stack[cnt]);
-            stack[cnt] = key;
-        }else {
-            stack[cnt_grow] = key;
-            cnt_grow++;
         }
+        stack[cnt] = key;
+        cnt = cnt==this.capacity-1 ? cnt-(this.capacity-1) : cnt+1;
         map.put(key, value);
     }
 }
 // 1324
 // 0123
+//["LRUCache","get","put","get","put","put","get","get"]
+//[[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
