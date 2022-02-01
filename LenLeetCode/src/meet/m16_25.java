6a5ed6dfa44d06626b51d95530c50310b7c03e1a
boyLenGit <1024552631@qq.com>
2022-02-01 12:26:07 +0800

    first commit

---
+++ LenLeetCode/src/meet/m16_25.java
@@ -0,0 +1,53 @@
+package meet;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class m16_25 {
+    public static void main(String[] arg){
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));       // 返回  1
+        cache.put(3, 3);                        // 该操作会使得密钥 2 作废
+        System.out.println(cache.get(2));       // 返回 -1 (未找到)
+        cache.put(4, 4);                        // 该操作会使得密钥 1 作废
+        System.out.println(cache.get(1));       // 返回 -1 (未找到)
+        System.out.println(cache.get(3));       // 返回  3
+        System.out.println(cache.get(4));       // 返回  4
+    }
+}
+
+class LRUCache {
+    private HashMap<Integer, Integer> map = new HashMap<>();
+    private int capacity;
+    private int cnt = 0;
+    private int[] stack;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        stack = new int[capacity];
+        for (int i1=0; i1<capacity; i1++) stack[i1] = i1;
+    }
+
+    public int get(int key) {
+        // 存stack
+        stack[this.cnt] = key;
+        this.cnt = this.cnt==this.capacity ? this.cnt-this.capacity : this.cnt++;
+        // 读map
+        if (!map.containsKey(key)) return -1;
+        return map.get(key);
+    }
+
+    public void put(int key, int value) {
+        if (map.size()==this.capacity){
+            cnt = cnt+1>=capacity ? 0 : cnt+1;
+            map.remove(stack[cnt]);
+        }
+        map.put(key, value);
+    }
+}
+// 1324
+// 0123
