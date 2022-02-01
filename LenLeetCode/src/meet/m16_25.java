6d93b698cc4b9dd9d240b2e0785f75c9507fcd94
boyLenGit <1024552631@qq.com>
2022-02-01 17:09:41 +0800

    first commit

--- LenLeetCode/src/meet/m16_25.java
+++ LenLeetCode/src/meet/m16_25.java
@@ -9,23 +9,23 @@ import java.util.Map;
 public class m16_25 {
     public static void main(String[] arg){
         LRUCache cache = new LRUCache(2);
-        cache.put(1, 1);
-        cache.put(2, 2);
-        System.out.println(cache.get(1));       // 返回  1
-        cache.put(3, 3);                        // 该操作会使得密钥 2 作废
-        System.out.println(cache.get(2));       // 返回 -1 (未找到)
-        cache.put(4, 4);                        // 该操作会使得密钥 1 作废
-        System.out.println(cache.get(1));       // 返回 -1 (未找到)
-        System.out.println(cache.get(3));       // 返回  3
-        System.out.println(cache.get(4));       // 返回  4
+//        cache.put(1, 1);
+//        cache.put(2, 2);
+//        System.out.println(cache.get(1));       // 返回  1
+//        cache.put(3, 3);                        // 该操作会使得密钥 2 作废
+//        System.out.println(cache.get(2));       // 返回 -1 (未找到)
+//        cache.put(4, 4);                        // 该操作会使得密钥 1 作废
+//        System.out.println(cache.get(1));       // 返回 -1 (未找到)
+//        System.out.println(cache.get(3));       // 返回  3
+//        System.out.println(cache.get(4));       // 返回  4
 
-//        System.out.println(cache.get(2));
-//        cache.put(2, 6);
-//        System.out.println(cache.get(1));
-//        cache.put(1, 5);
-//        cache.put(1, 2);
-//        System.out.println(cache.get(1));
-//        System.out.println(cache.get(2));
+        System.out.println(cache.get(2));
+        cache.put(2, 6);
+        System.out.println(cache.get(1));
+        cache.put(1, 5);
+        cache.put(1, 2);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(2));
     }
 }
 
@@ -52,10 +52,11 @@ class LRUCache {
     public void put(int key, int value) {
         if (map.size()==this.capacity){
             int i_min=0;
-            int min=0;
+            int min=9999999;
             for (Map.Entry<Integer, Integer> item: stack.entrySet()) {
                 if (item.getValue()<min) {
                     i_min = item.getKey();
+                    min = item.getValue();
                 }
             }
             map.remove(i_min);
