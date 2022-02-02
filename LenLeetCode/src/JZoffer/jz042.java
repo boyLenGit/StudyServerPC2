8c7c7317687b6753b5b07f6edaf7c056fa6ed278
boyLenGit <1024552631@qq.com>
2022-02-02 19:17:03 +0800

    first commit

--- LenLeetCode/src/JZoffer/jz042.java
+++ LenLeetCode/src/JZoffer/jz042.java
@@ -13,34 +13,10 @@ public class jz042 {
             } else sum = num;
             res = Math.max(res, sum);
         }
-        return sum;
-    }
-
-    public static int maxSubArray2(int[] nums) {
-        if (nums==null)  return 0;
-        int ans=nums[0]; // 遍历所有的情况中的最大和
-        int sum=0; // 当前子数组的和情况
-        // 遍历数组  如果有正数  那么最大和子数组应该是以正数开头正数结尾的
-        // 对于全都是负数的情况  那么遍历获取最大的负数就可以了
-        // 当正数开始  并且一直相加和>0的时候  允许你中间有部分容错（即中间存在部分负数）
-        // 此时记录遍历的最大和情况  即 ans=Math.max(sum,ans);
-        //如果和<=0了 那么代表容错没有了 加上后续的值无论后面的是正负哪种，都还不如不加
-        // 直接以后面的值作为新的子数组开头 继续遍历
-        for(int num:nums){
-            //如果和大于0  则加上遍历的数字  即使num<0也没事  允许其有部分容错
-            if (sum>0){
-                sum+=num;
-            }else{
-                // 和小于0了  那么就让和等于num  就代表从该值重新开启新的连续子数组
-                sum=num;
-            }
-            //保存获得此时最大和
-            ans=Math.max(sum,ans);
-        }
-        return ans;
+        return res;
     }
 
     public static void main(String[] arg){
-        System.out.println(maxSubArray2(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
     }
 }
--- LenLeetCode/src/lenTest/lt001_sorts.java
+++ LenLeetCode/src/lenTest/lt001_sorts.java
@@ -1,5 +1,7 @@
 package lenTest;
 
+import java.util.Arrays;
+
 public class lt001_sorts {
     public static void selectSort(int [] nums){
         int N=nums.length;
@@ -28,39 +30,39 @@ public class lt001_sorts {
     }
 
 
-    public static void quickSort(int[] arr,int low,int high){
-        int i,j,temp,t;
-        if(low>high) return;
-        i=low;
-        j=high;
+    public static void quickSort(int[] inputs,int start,int end){
+        int s, e, temp, t;
+        if(start>end) return;
+        s = start;
+        e = end;
         //temp就是基准位
-        temp = arr[low];
+        temp = inputs[start];
 
-        while (i<j) {
+        while (s<e) {
             //先看右边，依次往左递减
-            while (temp<=arr[j]&&i<j) j--;
+            while (temp<=inputs[e]&&s<e) e--;
             //再看左边，依次往右递增
-            while (temp>=arr[i]&&i<j) i++;
+            while (temp>=inputs[s]&&s<e) s++;
             //如果满足条件则交换
-            if (i<j) {
-                t = arr[j];
-                arr[j] = arr[i];
-                arr[i] = t;
+            if (s<e) {
+                t = inputs[e];
+                inputs[e] = inputs[s];
+                inputs[s] = t;
             }
         }
-        //最后将基准为与i和j相等位置的数字交换
-        arr[low] = arr[i];
-        arr[i] = temp;
+        //最后将基准为与s和e相等位置的数字交换
+        inputs[start] = inputs[s];
+        inputs[s] = temp;
         //递归调用左半数组
-        quickSort(arr, low, j-1);
+        quickSort(inputs, start, e-1);
         //递归调用右半数组
-        quickSort(arr, j+1, high);
+        quickSort(inputs, e+1, end);
     }
 
 
     public static void main(String[] a){
-        int[] inputs = new int[]{5,2,4,1,3,6,0};
+        int[] inputs = new int[]{5,2,4,1,1,3,6,0};
         quickSort(inputs, 0, inputs.length-1);
-        System.out.println(inputs);
+        System.out.println(Arrays.toString(inputs));
     }
 }
