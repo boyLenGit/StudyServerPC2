b1ba2951a48ec1c05e1c9a170f29790a4ad5caa2
boyLenGit <1024552631@qq.com>
2022-02-02 22:13:00 +0800

    first commit

---
+++ LenLeetCode/src/JZoffer/jz062.java
@@ -0,0 +1,20 @@
+package JZoffer;
+
+import java.util.ArrayList;
+
+public class jz062 {
+    public static int lastRemaining(int n, int m) {
+        ArrayList<Integer> dy = new ArrayList<>();
+        for (int i1=0; i1<n; i1++) dy.add(i1);
+        int index = m-1;
+        for (int i1=0; i1<n-1; i1++){
+            dy.remove(index);
+            index = index+(m-1)>=dy.size() ? index+(m-1)-dy.size(): index+(m-1);
+        }
+        return dy.get(0);
+    }
+
+    public static void main(String[] arg){
+        System.out.println(lastRemaining(5,3));
+    }
+}
