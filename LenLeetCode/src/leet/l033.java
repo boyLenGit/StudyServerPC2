65e6a7cac67baff04ba5a31ccb6a2358cd197c16
boyLenGit <1024552631@qq.com>
2022-02-04 23:46:28 +0800

    first commit

--- LenLeetCode/src/leet/l033.java
+++ LenLeetCode/src/leet/l033.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class l033 {
     public static int search(int[] nums, int target) {
-        int mid = 0, left_max=-99999, left_min=nums[0], right_max=nums[nums.length-1], right_min=99999;
+        if (nums.length==1) return nums[0]==target ? 0 : -1;
+        int mid = 0, left_max=-nums[nums.length-1], left_min=nums[0], right_max=nums[nums.length-1], right_min=nums[0];
         for (int i1=1; i1<nums.length; i1++){
             if (nums[i1-1]>nums[i1]){
                 mid = i1;
@@ -23,21 +24,35 @@ public class l033 {
         return res;
     }
 
-    public static int binSearch(int[] inputs, int target){
-        int low=0, high=inputs.length-1, middle=0;
-        while (low<=high){
-            middle = (low+high)/2;
-            if (inputs[middle]>target){
-                high = middle-1;
-            }else if (inputs[middle]<target){
-                high = middle+1;
-            }else return middle;
+    public static int binSearch(int[] nums, int target) {
+        int border_end=nums.length;
+        for (int i1=nums.length/2;;){
+            if (nums[i1]==target) return i1;
+            else if (nums[i1]>target) {
+                border_end = i1;
+                if (i1!=0){
+                    if (nums[i1-1]<target) break;
+                    i1 = border_end/2;
+                }else break;
+            }
+            else if (nums[i1]<target) {
+                if (i1!=nums.length-1){
+                    if (nums[i1+1]>target) break;
+                    i1 = i1 + ((border_end-i1)/2);
+                }else break;
+            }
+            // 放在最底部，用于防止index溢出
+            if (i1<0 | i1>=nums.length) break;
         }
         return -1;
     }
 
     public static void main(String[] arg){
-        int[] input1 = new int[]{4,5,6,7,0,1,2};
-        System.out.println(search(input1, 0));
+        int[] input1 = new int[]{8,1,2,3,4,5,6,7};
+        System.out.println(search(input1, 6));
     }
 }
+//[1,3]
+//1
+//[8,1,2,3,4,5,6,7]
+//6
