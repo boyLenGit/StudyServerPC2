96dd87c89528f88b7f484e749be4a7225ff08b1f
boyLenGit <1024552631@qq.com>
2022-02-06 17:18:10 +0800

    first commit

--- LenLeetCode/src/leet/l092.java
+++ LenLeetCode/src/leet/l092.java
@@ -1,11 +1,6 @@
 package leet;
 
 import leet.help.ListNode;
-import leet.help.TreeNode;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
 
 public class l092 {
     public static ListNode reverseBetween(ListNode head, int left, int right) {
@@ -21,20 +16,20 @@ public class l092 {
         for (int i1=0; ; i1++){
             if (i1!=0) head = head.next;
             if (head==null) break;
-
+            // 生成数组
             if (i1>=left-1 && i1<=right-1) array[cnt--] = head.val;
             else array[i1] = head.val;
         }
         ListNode res = null;
-        for (int i1=0; i1<array.length; i1++){
-            if (i1>=left-1 && i1<=right-1) res = new ListNode(array[cnt--], res);
-            else res = new ListNode(array[i1], res);
+        for (int i1=array.length-1; i1>=0; i1--){
+            res = new ListNode(array[i1], res);
         }
         return res;
     }
 
     public static void main(String[] ar){
         ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode listNode1 = new ListNode(1);
         System.out.println(reverseBetween(listNode, 2, 4).toString());
     }
 }
--- LenLeetCode/src/leet/l206.java
+++ LenLeetCode/src/leet/l206.java
@@ -14,4 +14,21 @@ public class l206 {
         }
         return res;
     }
+
+    public static ListNode reverseList2(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] ar){
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println(reverseList2(listNode).toString());
+    }
 }
