01a36281bc1f40a8025d2a0248fa92d4035b87f1
boyLenGit <1024552631@qq.com>
2022-02-06 11:00:32 +0800

    first commit

--- LenLeetCode/src/leet/l034.java
+++ LenLeetCode/src/leet/l034.java
@@ -14,17 +14,6 @@ public class l034 {
         return -1;
     }
 
-    public static int search1(int[] nums, int target) {
-        int low = 0, high = nums.length - 1, mid;
-        while (low <= high) {
-            mid = (high - low) / 2 + low;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) high = mid - 1;
-            else low = mid + 1;
-        }
-        return -1;
-    }
-
     public static int[] searchRange(int[] nums, int target) {
         int max, min;
         max = min = binarySearch(nums, target);
