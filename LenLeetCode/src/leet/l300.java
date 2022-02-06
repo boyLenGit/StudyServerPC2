8352162b0d2435d1851ab9b02f3350e815329e24
boyLenGit <1024552631@qq.com>
2022-02-06 22:37:15 +0800

    first commit

--- LenLeetCode/src/leet/l300.java
+++ LenLeetCode/src/leet/l300.java
@@ -1,22 +1,41 @@
 package leet;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class l300 {
     public static int lengthOfLIS(int[] nums) {
-        int cnt=0;
         int res=0;
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        int[] counts = new int[nums.length];
+        Arrays.fill(counts, 1);
         for (int i1=0; i1<nums.length; i1++){
-            for (int i2=0; i2<arrayList.size(); i2++){
-                if (nums[i1]>nums[i2]){
-                    arrayList.set(i2, arrayList.get(i2)+1);
-                    res = Math.max(arrayList.get(i2), res);
+            for (int i2=0; i2<i1; i2++){
+                if (nums[i2]<nums[i1]){
+                    counts[i1] = Math.max(counts[i1], counts[i2]+1);
                 }
             }
-            arrayList.add(1);
+            res = Math.max(res, counts[i1]);
         }
         return res;
+    }
+
+    public static int lengthOfLIS2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
     }
 
     public static void main(String[] arg){
