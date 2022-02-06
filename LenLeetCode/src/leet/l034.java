1bbc60d5dbfcd0c1928bd82d1b8229b62c39ae23
boyLenGit <1024552631@qq.com>
2022-02-06 10:46:01 +0800

    first commit

--- LenLeetCode/src/leet/l034.java
+++ LenLeetCode/src/leet/l034.java
@@ -3,15 +3,15 @@ package leet;
 import java.util.Arrays;
 
 public class l034 {
-    public static int binarySearch(int[] inputs, int target){
-        int low=0, high=inputs.length-1, mid;
+    public static int[] binarySearch(int[] inputs, int target){
+        int low=0, high=inputs.length-1, mid=-1;
         while (low<=high){
             mid = (high-low)/2 + low;
-            if (inputs[mid]==target) return mid;
+            if (inputs[mid]==target) return new int[]{mid, -1};
             else if (inputs[mid]>target) high = mid-1;
             else low = mid+1;
         }
-        return -1;
+        return new int[]{mid, -1};
     }
 
     public static int[] searchRange(int[] nums, int target) {
@@ -22,7 +22,7 @@ public class l034 {
             mid = (high-low)/2 + low;
             if (nums[mid]==target) {
                 res[cnt] = mid;
-                cnt ++;
+                cnt = 1;
             }
             else if (nums[mid]>target) high = mid-1;
             else low = mid+1;
@@ -33,6 +33,6 @@ public class l034 {
 
     public static void main(String[] a){
         int[] inputs = new int[]{5,7,7,8,8,10};
-        System.out.println(Arrays.toString(searchRange(inputs, 8)));
+        System.out.println(Arrays.toString(binarySearch(inputs, 8)));
     }
 }
