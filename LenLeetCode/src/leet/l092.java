d8cdaeac0e8daf0f11c12ac320fd3a982c8fc03c
boyLenGit <1024552631@qq.com>
2022-02-06 16:42:31 +0800

    first commit

---
+++ LenLeetCode/src/leet/l092.java
@@ -0,0 +1,41 @@
+package leet;
+
+import leet.help.ListNode;
+import leet.help.TreeNode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class l092 {
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
+        int depth = 0;
+        ListNode temp = head;
+        for (int i1=0; ; i1++){
+            if (i1!=0) temp = temp.next;
+            if (temp==null) break;
+            depth++;
+        }
+        int[] array = new int[depth];
+        int[] array_temp = new int[right-left+1];
+        int cnt = 0;
+        for (int i1=0; ; i1++){
+            if (i1!=0) head = head.next;
+            if (head==null) break;
+            if (i1>=left-1 && i1<=right-1) array_temp[cnt++] = head.val;
+            array[i1] = head.val;
+        }
+//        Arrays.sort(array, left-1, right-1);
+        ListNode res = null;
+        for (int i1=0; i1<array.length; i1++){
+            if (i1>=left-1 && i1<=right-1) res = new ListNode(array[i1]);
+            res = new ListNode(array[i1]);
+        }
+        return res;
+    }
+
+    public static void main(String[] ar){
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println(reverseBetween(listNode, 2, 4).toString());
+    }
+}
