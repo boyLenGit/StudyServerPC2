dccac3951ffde777eb0bb5101a87f5af0f8657ab
boyLenGit <1024552631@qq.com>
2022-02-06 10:59:45 +0800

    first commit

--- LenLeetCode/src/leet/l034.java
+++ LenLeetCode/src/leet/l034.java
@@ -3,36 +3,46 @@ package leet;
 import java.util.Arrays;
 
 public class l034 {
-    public static int[] binarySearch(int[] inputs, int target){
-        int low=0, high=inputs.length-1, mid=-1;
+    public static int binarySearch(int[] inputs, int target){
+        int low=0, high=inputs.length-1, mid;
         while (low<=high){
             mid = (high-low)/2 + low;
-            if (inputs[mid]==target) return new int[]{mid, -1};
+            if (inputs[mid]==target) return mid;
             else if (inputs[mid]>target) high = mid-1;
             else low = mid+1;
         }
-        return new int[]{mid, -1};
+        return -1;
+    }
+
+    public static int search1(int[] nums, int target) {
+        int low = 0, high = nums.length - 1, mid;
+        while (low <= high) {
+            mid = (high - low) / 2 + low;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
+        }
+        return -1;
     }
 
     public static int[] searchRange(int[] nums, int target) {
-        int[] res = new int[]{-1, -1};
-        int cnt = 0;
-        int low=0, high=nums.length-1, mid;
-        while (low<=high){
-            mid = (high-low)/2 + low;
-            if (nums[mid]==target) {
-                res[cnt] = mid;
-                cnt = 1;
-            }
-            else if (nums[mid]>target) high = mid-1;
-            else low = mid+1;
+        int max, min;
+        max = min = binarySearch(nums, target);
+        if (max==-1) return new int[]{min, max};
+        for (int i1=max; i1<nums.length; i1++){
+            if (nums[i1]==target) max = i1;
+            else break;
         }
-        if (res[1]==-1 & res[0]!=-1) res[1]=res[0];
-        return res;
+        for (int i1=min; i1>=0; i1--){
+            if (nums[i1]==target) min = i1;
+            else break;
+        }
+        return new int[]{min, max};
     }
 
     public static void main(String[] a){
         int[] inputs = new int[]{5,7,7,8,8,10};
-        System.out.println(Arrays.toString(binarySearch(inputs, 8)));
+        System.out.println(Arrays.toString(searchRange(inputs, 6)));
+        System.out.println(search1(inputs, 8));
     }
 }
