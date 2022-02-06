44d512ad6e66670e77da76f899cec94af19e0f3d
boyLenGit <1024552631@qq.com>
2022-02-06 16:09:32 +0800

    first commit

--- LenLeetCode/src/leet/l543.java
+++ LenLeetCode/src/leet/l543.java
@@ -6,27 +6,26 @@ import sun.jvm.hotspot.oops.Array;
 import java.util.ArrayList;
 
 public class l543 {
+    private static int max;
     public static int diameterOfBinaryTree(TreeNode root) {
-        ArrayList<TreeNode> inputs = new ArrayList<>();
-        ArrayList<TreeNode> temp;
-        inputs.add(root);
-        int i1=0;
-        for (; ; i1++){
-            temp = new ArrayList<>();
-            for (int i2=0; i2<inputs.size(); i2++){
-                if (inputs.get(i2).left!=null) temp.add(inputs.get(i2).left);
-                if (inputs.get(i2).right!=null) temp.add(inputs.get(i2).right);
-            }
-            if (temp.size()==0) break;
-            inputs = temp;
-        }
-        return i1;
+        if (root==null) return 0;
+        DFX(root);
+        return max;
+    }
+
+    public static int DFX(TreeNode input){
+        int len_left=0, len_right=0;
+        len_left = input.left==null ? len_left : DFX(input.left)+1;
+        len_right = input.right==null ? len_right : DFX(input.right)+1;
+        max = Math.max(max, len_left+len_right);
+        return Math.max(len_left, len_right);
     }
 
     public static void main(String[] a){
         TreeNode input1 = new TreeNode(3,
                 new TreeNode(9),
                 new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-        System.out.println(diameterOfBinaryTree(input1));
+        TreeNode input2 = new TreeNode(1, new TreeNode(2), null);
+        System.out.println(diameterOfBinaryTree(input2));
     }
 }
