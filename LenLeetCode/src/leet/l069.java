c0671e666f789af35092c743d329447468613cb0
boyLenGit <1024552631@qq.com>
2022-02-07 20:42:36 +0800

    first commit

--- LenLeetCode/src/leet/l069.java
+++ LenLeetCode/src/leet/l069.java
@@ -8,15 +8,16 @@ public class l069 {
     public static int mySqrt2(int x) {
         int low=0, high=x-1, mid=0;
         while (low<=high){
-            mid = (high-low)/2 +low;
+            mid = (high-low)/2 + low;
             if (mid*mid<=x && (mid+1)*(mid+1)>x) return mid;
-            else if (mid*mid<x && (mid+1)*(mid+1)<x) high = mid+1;
-            else if (mid*mid>x) low = mid-1;
+            else if ((mid+1)*(mid+1)==x) return mid+1;
+            else if (mid*mid<x) low = mid-1;
+            else if (mid*mid>x) high = mid+1;
         }
         return mid;
     }
 
     public static void main(String[] arg){
-        System.out.println(mySqrt2(8));
+        System.out.println(mySqrt2(4));
     }
 }
