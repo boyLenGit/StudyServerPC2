661fcc9f38e092886c82745f8a980c960d733cb1
boyLenGit <1024552631@qq.com>
2022-02-08 19:37:41 +0800

    first commit

--- LenLeetCode/src/JZoffer/jz025.java
+++ LenLeetCode/src/JZoffer/jz025.java
@@ -4,6 +4,7 @@ import leet.help.ListNode;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 
 public class jz025 {
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -26,9 +27,60 @@ public class jz025 {
         return res;
     }
 
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        // 改用LinkedList
+        LinkedList<Integer> inputs = new LinkedList<>();
+        for (int i1=0; ; i1++){
+            if (i1!=0) l1 = l1.next;
+            if (l1==null)break;
+            inputs.add(l1.val);
+        }
+        for (int i1=0; ; i1++){
+            if (i1!=0) l2 = l2.next;
+            if (l2==null)break;
+            inputs.add(l2.val);
+        }
+        Collections.sort(inputs);
+        ListNode res = null;
+        for (int i1=inputs.size()-1; i1>=0; i1--){
+            res = new ListNode(inputs.get(i1), res);
+        }
+        return res;
+    }
+
+    public static ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        // 认真读了题, 根据"两个递增排序的链表"来做
+        boolean isUsed = false;
+        ListNode temp;
+        ListNode l2_mask = l2;
+        for (int i1=0; ; i1++){
+            if (i1!=0 & l2_mask.next!=null) l2_mask = l2_mask.next;
+            if (isUsed) {
+                l1 = l1.next;
+                isUsed = false;
+            }
+            if (l1==null) break;
+
+            if (l2_mask.val<l1.val){
+                if (l2_mask.next!=null && l2_mask.next.val>l1.val){
+                    temp = l2_mask.next;
+                    l2_mask = new ListNode(l1.val, temp);
+                    isUsed = true;
+                }else if (l2_mask.next==null){
+                    l2_mask.next = new ListNode(l1.val);
+                    isUsed = true;
+                }
+            }else if (l2_mask.val==l1.val){
+                l2_mask.next = new ListNode(l1.val, l2_mask);
+                isUsed = true;
+            }
+        }
+        return l2;
+    }
+
     public static void main(String[] arg){
         ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        System.out.println(mergeTwoLists(null, new ListNode(1)));
+        System.out.println( mergeTwoLists3(listNode1, listNode2) );
     }
 }
