771edad1aabb42996b0400af1b94e4db26b15e50
boyLenGit <1024552631@qq.com>
2022-02-09 15:36:25 +0800

    first commit

---
+++ LenLeetCode/src/JZoffer/jz040.java
@@ -0,0 +1,39 @@
+package JZoffer;
+
+import java.util.Arrays;
+
+public class jz040 {
+    public static int[] getLeastNumbers(int[] arr, int k) {
+        lenQuickSortDG(arr, 0, arr.length-1);
+        return Arrays.copyOfRange(arr, 0, k);
+    }
+
+    public static void lenQuickSortDG(int[] inputs, int start, int end){
+        int i1=start, i2=end, exchange, compare;
+        if (start>end)return;
+        compare = inputs[start];
+        while (i1<i2){
+            while (compare>=inputs[i2] && i1<i2) i2--;
+            while (compare<=inputs[i1] && i1<i2) i1++;
+            if (i1<i2){
+                exchange = inputs[i1];
+                inputs[i1] = inputs[i2];
+                inputs[i2] = exchange;
+            }
+        }
+        inputs[start] = inputs[i1];
+        inputs[i1] = compare;
+        lenQuickSortDG(inputs, start, i1-1);
+        lenQuickSortDG(inputs, i1+1, end);
+    }
+
+    public static int[] getLeastNumbers2(int[] arr, int k) {
+        Arrays.sort(arr);
+        return Arrays.copyOfRange(arr, 0, k);
+    }
+
+    public static void main(String[] arg){
+        int[] inputs = new int[]{3,2,1};
+        System.out.println(Arrays.toString(getLeastNumbers(inputs, 2)));
+    }
+}
--- LenLeetCode/src/leet/l141.java
+++ LenLeetCode/src/leet/l141.java
@@ -2,16 +2,33 @@ package leet;
 
 import leet.help.ListNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class l141 {
     public static boolean hasCycle(ListNode head) {
-        int depth=0;
+        Set<Integer> set = new HashSet<>();
         for (int i1=0; ; i1++){
             if (i1!=0) head = head.next;
             if (head==null) break;
-            System.out.println(head.getClass());
-            depth++;
+            if (set.contains(head.hashCode())) return true;
+            set.add(head.hashCode());
         }
-        return true;
+        return false;
+    }
+
+    public static boolean hasCycle2(ListNode head) {
+        // 快慢指针法
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i1=0; ; i1++){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast==null) break;
+            if (slow==fast) return true;
+
+        }
+        return false;
     }
 
     public static void main(String[] arg){
