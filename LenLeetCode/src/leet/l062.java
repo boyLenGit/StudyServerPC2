ed73cdcbaade7a2fa2a71fee33530f9421807adc
boyLenGit <1024552631@qq.com>
2022-02-13 09:31:17 +0800

    first commit

--- LenLeetCode/src/leet/l062.java
+++ LenLeetCode/src/leet/l062.java
@@ -5,18 +5,22 @@ import java.util.Arrays;
 public class l062 {
     private int cnt = 0;
     public int uniquePaths(int m, int n) {
-        dg(m,n,0,0);
+        dg(m-1,n-1,0,0);
         return cnt;
     }
 
     public  void dg(int m, int n, int init_m, int init_n){
-        if (init_m>=m | init_n>=n) {
+        if (init_m==m && init_n==n) {
             System.out.println(Arrays.toString(new int[]{init_m, init_n}));
             cnt++;
-            return;
+        }else if (init_m==m){
+            dg(m,n,init_m,init_n+1);
+        }else if (init_n==n){
+            dg(m,n,init_m+1,init_n);
+        }else {
+            dg(m,n,init_m,init_n+1);
+            dg(m,n,init_m+1,init_n);
         }
-        dg(m,n,init_m+1,init_n);
-        dg(m,n,init_m,init_n+1);
     }
 
     public static void main(String[] arg){
