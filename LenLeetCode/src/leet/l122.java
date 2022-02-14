3e3403644e765854a47c1b957abb8719f0a0fea7
boyLenGit <1024552631@qq.com>
2022-02-14 21:05:38 +0800

    first commit

--- LenLeetCode/src/leet/l122.java
+++ LenLeetCode/src/leet/l122.java
@@ -3,15 +3,14 @@ package leet;
 public class l122 {
     public static int maxProfit(int[] prices) {
         int sum=0, sum_max=0, res = 0;
-        for (int i1=1, i2=i1; i2<prices.length; i2++){
-            if (sum<=0) {
-                res += sum_max;
-                sum_max=0;
-                sum = prices[i2]-prices[i2-1];
+        for (int i1=1; i1<prices.length; i1++){
+            if (prices[i1]-prices[i1-1]>=0){
+                sum += prices[i1]-prices[i1-1];
             }else {
-                sum += prices[i2]-prices[i2-1];
+                sum = 0;
+                res += sum_max;
             }
-            sum_max = Math.max(sum, sum_max);
+            sum_max = Math.max(sum_max, sum);
         }
         return res;
     }
