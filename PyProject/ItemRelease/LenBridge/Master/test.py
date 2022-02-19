4b718116a3a8f9a2c143ed7edb69b51e5978364a
boyLenGit <1024552631@qq.com>
2022-02-19 10:55:44 +0800

    first commit

--- PyProject/ItemRelease/LenBridge/Master/test.py
+++ PyProject/ItemRelease/LenBridge/Master/test.py
@@ -6,9 +6,11 @@ def test_long_2vlist():
     list2 = []
     for i1 in range(100):
         for i2 in range(200):
-            list2.append(random.random)
+            list2.append(i2)
         list1.append(list2)
-    print(list1)
+    result = bridgeMaster.scheduler("list2vSquare", list1)
+    print(list1[5])
+    print(result[5])
 
 
 if __name__ == '__main__':
