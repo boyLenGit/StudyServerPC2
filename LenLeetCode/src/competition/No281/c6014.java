9d92e6e14436eb663135f3181cbce4a06aa28015
boyLenGit <1024552631@qq.com>
2022-02-20 11:18:21 +0800

    first commit

--- LenLeetCode/src/competition/No281/c6014.java
+++ LenLeetCode/src/competition/No281/c6014.java
@@ -9,20 +9,42 @@ public class c6014 {
             inputs[i1] = s.charAt(i1);
         }
         Arrays.sort(inputs);
-        int repeatCount = 0;
+        int repeatCount = 1;
         char exchange;
+        // 逆向
         for (int i1=inputs.length-1; i1>=0; i1--){
             if (i1!=inputs.length-1){
                 if (inputs[i1]==inputs[i1+1]){
                     repeatCount++;
-                }else repeatCount=0;
+                }else repeatCount=1;
             }
             if (repeatCount>repeatLimit){
-                exchange = inputs[i1];
-                inputs[i1] = inputs[i1+1];
-                inputs[i1+1] = exchange;
+                for (int i2=i1; i2>=0; i2--){
+                    if (inputs[i2]!=inputs[i1]){
+                        exchange = inputs[i1];
+                        inputs[i1] = inputs[i2];
+                        inputs[i2] = exchange;
+                    }
+                }
             }
         }
+//        // 正向
+//        for (int i1=0; i1<inputs.length; i1++){
+//            if (i1!=0){
+//                if (inputs[i1]==inputs[i1-1]){
+//                    repeatCount++;
+//                }else repeatCount=1;
+//            }
+//            if (repeatCount>repeatLimit){
+//                for (int i2=i1; i2<inputs.length; i2++){
+//                    if (inputs[i2]!=inputs[i1]){
+//                        exchange = inputs[i1];
+//                        inputs[i1] = inputs[i2];
+//                        inputs[i2] = exchange;
+//                    }
+//                }
+//            }
+//        }
         StringBuilder sb = new StringBuilder();
         for (int i1=inputs.length-1; i1>=0; i1--){
             sb.append(inputs[i1]);
@@ -31,6 +53,6 @@ public class c6014 {
     }
 
     public static void main(String[] arg){
-        System.out.println(repeatLimitedString("cczazcc", 3));
+        System.out.println(repeatLimitedString("aababab", 2));
     }
 }
