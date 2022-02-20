441e1474b0e36e0e1f2b15aec6e580279c4ed7ab
boyLenGit <1024552631@qq.com>
2022-02-20 10:55:56 +0800

    first commit

---
+++ LenLeetCode/src/competition/No281/c6012.java
@@ -0,0 +1,25 @@
+package competition.No281;
+
+public class c6012 {
+    public static int countEven(int num) {
+        int res = 0;
+        int temp = 0;
+        int sum;
+        for (int i1=1; i1<=num; i1++){
+            sum=0;
+            temp=i1;
+            while (temp==0){
+                sum += temp%10;
+                temp = temp/10;
+            }
+            if (sum%2==0) res++;
+        }
+        return res;
+    }
+
+    public static void main(String[] arg){
+        System.out.println(countEven(4));
+    }
+
+
+}
