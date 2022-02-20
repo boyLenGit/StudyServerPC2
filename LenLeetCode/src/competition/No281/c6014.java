4d93d74014c23bb183e9bd980f97546f5deeb187
boyLenGit <1024552631@qq.com>
2022-02-20 12:06:43 +0800

    first commit

--- LenLeetCode/src/competition/No281/c6014.java
+++ LenLeetCode/src/competition/No281/c6014.java
@@ -7,7 +7,7 @@ public class c6014 {
         char[] inputs = s.toCharArray();
         Arrays.sort(inputs);
         int repeatCount = 1;
-        int index_repeat = 0;
+        int index_repeat = s.length()-1;
         char exchange;
         for (int i1=inputs.length-1; i1>=0; i1--){
             if (i1!=inputs.length-1 && inputs[i1+1]!='0'){
@@ -16,13 +16,15 @@ public class c6014 {
             }
             if (repeatCount>repeatLimit){
                 boolean isEdge = true;
-                for (int i2=i1-1; i2>=0; i2--){
+                index_repeat = Math.min(index_repeat, i1 - 1);
+                for (int i2=index_repeat; i2>=0; i2--){
                     if (inputs[i2]!=inputs[i1]){
                         exchange = inputs[i1];
                         inputs[i1] = inputs[i2];
                         inputs[i2] = exchange;
                         isEdge = false;
                         repeatCount = 1;
+                        index_repeat = i2+1;
                         break;
                     }
                 }
@@ -39,7 +41,7 @@ public class c6014 {
 
     public static void main(String[] arg){
         System.out.println("zyzyzyxyxyxyxwxwxwxvxvxuxututststsrsrsrqrqrpopopopopopopononmnmlklkljljljijijijhghghghghfhfefefdfdfcfcbab");
-        System.out.println(repeatLimitedString("aaaaaabbbbbbb", 1));
+        System.out.println(repeatLimitedString("zyzyzyxyxyxyxwxwxwxvxvxuxututststsrsrsrqrqrpopopopopopopononmnmlklkljljljijijijhghghghghfhfefefdfdfcfcbab", 1));
     }
 }
 //"robnsdvpuxbapuqgopqvxdrchivlifeepy"
