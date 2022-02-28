9d2cfe1e1293b37d6fbee401fa659a69a5aa82da
boyLenGit <1024552631@qq.com>
2022-02-28 23:48:43 +0800

    first commit

--- LenLeetCode/src/leet/l198.java
+++ LenLeetCode/src/leet/l198.java
@@ -4,9 +4,44 @@ public class l198 {
     public static int rob(int[] nums) {
         int ret = 0;
         for (int i1=0; i1<nums.length; i1++){
-            if (i1+1<nums.length && nums[i1]<nums[i1+1]){
+            if (i1+3<nums.length && nums[i1]+nums[i1+2]<nums[i1+1]+nums[i1+3]){
                 ret += nums[i1+1];
                 i1=i1+2;
+            }else if (i1+3==nums.length && nums[i1+1]>=nums[i1+2]+nums[i1]){
+                ret += nums[i1+1];
+                i1=i1+2;
+            }else if (i1+2==nums.length && nums[i1]<nums[i1+1]){
+                ret += nums[i1+1];
+                i1++;
+            }else {
+                ret += nums[i1];
+                i1++;
+            }
+        }
+        return ret;
+    }
+
+    public static int rob2(int[] nums) {
+        int ret = 0;
+        for (int i1=0; i1<nums.length; i1++){
+            if (i1+3<nums.length){
+                int s_0_2 = nums[i1]+nums[i1+2];
+                int s_0_3 = nums[i1]+nums[i1+3];
+                int s_1_3 = nums[i1+1]+nums[i1+3];
+                int max = Math.max(Math.max(s_0_2, s_0_3), s_1_3);
+                if (max==s_0_3) {
+                    ret += nums[i1] + nums[i1+3];
+                    i1=i1+2+2;
+                }else if (max==s_1_3){
+                    ret += nums[i1+1];
+                    i1=i1+2;
+                }
+            }else if (i1+3==nums.length && nums[i1+1]>=nums[i1+2]+nums[i1]){
+                ret += nums[i1+1];
+                i1=i1+2;
+            }else if (i1+2==nums.length && nums[i1]<nums[i1+1]){
+                ret += nums[i1+1];
+                i1++;
             }else {
                 ret += nums[i1];
                 i1++;
@@ -16,7 +51,10 @@ public class l198 {
     }
 
     public static void main(String[] arg){
-        int[] input1 = new int[]{1,2,3,1};
-        System.out.println(rob(input1));
+        int[] input1 = new int[]{1,7,9,4};
+        System.out.println(rob2(input1));
     }
 }
+//[1,2,1,1]
+// [1,7,9,4] 1,7,9,1
+// [4,1,2,7,5,3,1]
