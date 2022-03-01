23aff47c30db0a8fc85e3583bd7608d875a6d2d2
boyLenGit <1024552631@qq.com>
2022-03-01 10:31:31 +0800

    first commit

--- LenLeetCode/src/leet/l198.java
+++ LenLeetCode/src/leet/l198.java
@@ -31,10 +31,13 @@ public class l198 {
                 int max = Math.max(Math.max(s_0_2, s_0_3), s_1_3);
                 if (max==s_0_3) {
                     ret += nums[i1] + nums[i1+3];
-                    i1=i1+2+2;
+                    i1=i1+5-1;
                 }else if (max==s_1_3){
                     ret += nums[i1+1];
                     i1=i1+2;
+                }else {
+                    ret += nums[i1] + nums[i1+2];
+                    i1=i1+4-1;
                 }
             }else if (i1+3==nums.length && nums[i1+1]>=nums[i1+2]+nums[i1]){
                 ret += nums[i1+1];
@@ -50,11 +53,32 @@ public class l198 {
         return ret;
     }
 
+    public static int rob3(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+
     public static void main(String[] arg){
-        int[] input1 = new int[]{1,7,9,4};
-        System.out.println(rob2(input1));
+        int[] input1 = new int[]{1,2,3,1};
+        System.out.println(rob3(input1));
     }
 }
-//[1,2,1,1]
+//[1,2,3,1]
+//[2,7,9,3,1]
+// [1,2,1,1]
 // [1,7,9,4] 1,7,9,1
 // [4,1,2,7,5,3,1]
+// [2,4,8,9,9,3]
+//  1   1   1
