d00f42960322fd1d35a536e6b0535eadd4fe4c0e
boyLenGit <1024552631@qq.com>
2022-03-17 16:19:00 +0800

    first commit

--- cloud02/geodata_service/src/test/java/len/cloud02/geodata_service/GeodataServiceApplicationTests.java
+++ cloud02/geodata_service/src/test/java/len/cloud02/geodata_service/GeodataServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package len.cloud02.geodata_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class GeodataServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package len.cloud02.geodata_service;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GeodataServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
