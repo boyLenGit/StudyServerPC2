73267c5f16c842722bb5d87333a0646c4afda0e8
boyLenGit <1024552631@qq.com>
2022-03-17 15:42:51 +0800

    first commit

--- cloud02/geodata_service/src/main/java/len/cloud02/geodata_service/GeodataServiceApplication.java
+++ cloud02/geodata_service/src/main/java/len/cloud02/geodata_service/GeodataServiceApplication.java
@@ -1,8 +1,10 @@
 package len.cloud02.geodata_service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("len.cloud02.geodata_service.mapper")
 @SpringBootApplication
 public class GeodataServiceApplication {
 
