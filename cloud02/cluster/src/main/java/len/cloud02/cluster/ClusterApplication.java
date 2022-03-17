ccf31de71c65195be6ae4bd6ca391de0c918c852
boyLenGit <1024552631@qq.com>
2022-03-17 17:19:03 +0800

    first commit

---
+++ cloud02/cluster/src/main/java/len/cloud02/cluster/ClusterApplication.java
@@ -0,0 +1,13 @@
+package len.cloud02.cluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClusterApplication.class, args);
+    }
+
+}
---
+++ cloud02/cluster/src/test/java/len/cloud02/cluster/ClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package len.cloud02.cluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
