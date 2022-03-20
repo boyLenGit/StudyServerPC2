378f583a70fa50431629c412b84bb4bc425a2eae
boyLenGit <1024552631@qq.com>
2022-03-20 23:18:36 +0800

    first commit

--- LenLeetCode/src/leet/l237.java
+++ LenLeetCode/src/leet/l237.java
@@ -2,6 +2,8 @@ package leet;
 
 import leet.help.ListNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class l237 {
@@ -15,5 +17,19 @@ public class l237 {
         l237 l = new l237();
         l.deleteNode(in);
         System.out.println(in.toString());
+
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        arrayList1.add(123);
+        arrayList1.add(432);
+        int[] ints1 = new int[arrayList1.size()];
+        for (int i1=0; i1<arrayList1.size(); i1++){
+            ints1[i1] = arrayList1.get(i1);
+        }
+        System.out.println(Arrays.toString(ints1));
+
+
+
+        long a = 10l;
+        int b = (int) a;
     }
 }
