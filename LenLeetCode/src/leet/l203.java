75404ee2203884637abfc1a8d54f6f2020c5d41a
boyLenGit <1024552631@qq.com>
2022-03-26 22:40:54 +0800

    first commit

--- LenLeetCode/src/leet/l203.java
+++ LenLeetCode/src/leet/l203.java
@@ -1,2 +1,31 @@
-package leet;public class l203 {
+package leet;
+
+import leet.help.ListNode;
+
+public class l203 {
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode res = head;
+        while (head!=null && head.val==val){
+            if (head.next!=null){
+                head.val = head.next.val;
+                head.next = head.next.next;
+            }else return null;
+        }
+        while (head!=null && head.next!=null){
+            if (head.next.val==val){
+                if (head.next.next!=null){
+                    head.next = head.next.next;
+                    continue;
+                }else head.next=null;
+            }
+            head = head.next;
+        }
+//        if (res!=null && res.val==val && res.next==null) return null;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        System.out.println(removeElements(listNode1, 1));
+    }
 }
--- LenLeetCode/src/leet/l235.java
+++ LenLeetCode/src/leet/l235.java
@@ -3,14 +3,35 @@ package leet;
 import leet.help.TreeNode;
 
 public class l235 {
-//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-//
-//    }
+    TreeNode res;
+    int lock=0;
 
-    public void dg(TreeNode root, TreeNode p, TreeNode q){
-        if (root==p) return;
-        if (root.left!=null) dg(root.left, p, q);
-        if (root.right!=null) dg(root.right, p, q);
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int state_left=0, state_right=0;
+        dg(root, p, q);
+        return res;
+    }
+
+    public int dg(TreeNode root, TreeNode p, TreeNode q){
+        int state_left=0, state_right=0;
+        if (root.left!=null) state_left = dg(root.left, p, q);
+        if (root.right!=null) state_right = dg(root.right, p, q);
+        if (root==p || root==q) {
+            if (state_right+state_left==1 && lock==0) res = root;
+            return 1;
+        }
+        if (state_right+state_left>=2 && lock==0) {
+            res = root;
+            lock=1;
+        }
+        return state_left+state_right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode q1 = new TreeNode(4);
+        TreeNode p1 = new TreeNode(2, q1, null);
+        l235 l = new l235();
+        l.lowestCommonAncestor(p1, p1, q1);
 
     }
 }
