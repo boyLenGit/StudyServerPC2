912d3cc7a42cdd5ad8a80e2200b6e88d1925bc65
boyLenGit <1024552631@qq.com>
2022-04-15 09:57:58 +0800

    first commit

--- cloud02/blog/src/main/java/len/cloud02/blog/BlogApplication.java
+++ cloud02/blog/src/main/java/len/cloud02/blog/BlogApplication.java
@@ -11,13 +11,13 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(BlogApplication.class, args);
-        // 打印所有bean的信息
-        int bean_num = applicationContext.getBeanDefinitionCount();
-        String[] bean_name = applicationContext.getBeanDefinitionNames();
-        for (String item: bean_name){
-            LenLog.staticInfo("main", item);
-        }
-        LenLog.staticInfo("main", String.valueOf(bean_num));
+//        // 打印所有bean的信息
+//        int bean_num = applicationContext.getBeanDefinitionCount();
+//        String[] bean_name = applicationContext.getBeanDefinitionNames();
+//        for (String item: bean_name){
+//            LenLog.staticInfo("main", item);
+//        }
+//        LenLog.staticInfo("main", String.valueOf(bean_num));
     }
 
 }
