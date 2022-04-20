e690ce4e9f50d549520fa5cc3c2e427f78f43951
boyLenGit <1024552631@qq.com>
2022-04-20 22:09:13 +0800

    first commit

--- cloud02/blog/src/test/java/len/cloud02/blog/ReadFile.java
+++ cloud02/blog/src/test/java/len/cloud02/blog/ReadFile.java
@@ -15,7 +15,42 @@ public class ReadFile {
     public static void readXML() throws IOException {
         File xmlFile = new File("/Users/mabolun/Project/Java/cloud02/blog/src/test/java/len/cloud02/blog/ReadFile_json.config");
         String config = FileUtil.readAsString(xmlFile);
-        Plugins plugins = JSON.parseObject(config, Plugins.class);
-        System.out.println(plugins);
+        Person person = JSON.parseObject(config, Person.class);
+
+        System.out.println(person);
+    }
+
+    class Person{
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
 }
