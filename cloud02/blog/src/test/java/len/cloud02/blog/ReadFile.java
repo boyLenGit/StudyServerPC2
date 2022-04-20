7b3b2318f5590c96097afaa3cacc9bde673b00ae
boyLenGit <1024552631@qq.com>
2022-04-20 22:00:28 +0800

    first commit

--- cloud02/blog/src/test/java/len/cloud02/blog/ReadFile.java
+++ cloud02/blog/src/test/java/len/cloud02/blog/ReadFile.java
@@ -1,11 +1,21 @@
 package len.cloud02.blog;
 
-public class ReadFile {
-    public static void main(String[] args) {
+import com.alibaba.fastjson.JSON;
+import org.aspectj.util.FileUtil;
+import org.mockito.internal.configuration.plugins.Plugins;
 
+import java.io.File;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) throws IOException {
+        readXML();
     }
 
-    public static void readXML(){
-        
+    public static void readXML() throws IOException {
+        File xmlFile = new File("/Users/mabolun/Project/Java/cloud02/blog/src/test/java/len/cloud02/blog/ReadFile_json.config");
+        String config = FileUtil.readAsString(xmlFile);
+        Plugins plugins = JSON.parseObject(config, Plugins.class);
+        System.out.println(plugins);
     }
 }
