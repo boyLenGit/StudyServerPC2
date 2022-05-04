2bb86f410bdaa54222a5c06bf892c4ed66a7530e
boyLenGit <1024552631@qq.com>
2022-05-04 22:53:49 +0800

    first commit

--- library01/src/main/java/com/len/library/library01/service/BookService.java
+++ library01/src/main/java/com/len/library/library01/service/BookService.java
@@ -3,6 +3,8 @@ package com.len.library.library01.service;
 import com.len.library.library01.dao.BookRepository;
 import com.len.library.library01.pojo.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +14,8 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public List<Book> getBookList(){
-        List<Book> books = bookRepository.findAll();
+    public Page<Book> getBookList(Pageable pageable){
+        Page<Book> books = bookRepository.findAll(pageable);
         return books;
     }
 }
--- library01/src/main/java/com/len/library/library01/web/bookController.java
+++ library01/src/main/java/com/len/library/library01/web/bookController.java
@@ -3,6 +3,10 @@ package com.len.library.library01.web;
 import com.len.library.library01.pojo.Book;
 import com.len.library.library01.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +19,10 @@ public class bookController {
     private BookService bookService;
 
     @GetMapping("/books")
-    public String getBookListController(Model model){
-        List<Book> books = bookService.getBookList();
+    public String getBookListController(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable,
+                                        Model model)
+    {
+        Page<Book> books = bookService.getBookList(pageable);
         model.addAttribute("books", books);
         return "book_list";
     }
