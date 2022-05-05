03a6968ddee0b4f6793bf75510870e7658882f18
boyLenGit <1024552631@qq.com>
2022-05-05 14:14:08 +0800

    first commit

--- library01/src/main/java/com/len/library/library01/web/bookController.java
+++ library01/src/main/java/com/len/library/library01/web/bookController.java
@@ -2,7 +2,6 @@ package com.len.library.library01.web;
 
 import com.len.library.library01.pojo.Book;
 import com.len.library.library01.service.BookService;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class bookController {
@@ -32,15 +30,15 @@ public class bookController {
         return "book_list";
     }
 
-    @GetMapping("/books/add")
+    @GetMapping("/book_add")
     public String addBook_jump(Model model){
         Book book = new Book();
         book.setName("博客名称测试");
         model.addAttribute("book", book);
-        return "add_book";
+        return "book_add";
     }
 
-    @PostMapping("/books/add_main")
+    @PostMapping("/book_add")
     public String addBook(Book book, RedirectAttributes redirectAttributes, HttpSession httpSession){
         System.out.println("LenTest-addBook: " + book.toString());
         bookService.addBook(book);
