33ef522865dc7ee17c73c3d113f8efeeb9682c91
boyLenGit <1024552631@qq.com>
2022-05-10 18:59:08 +0800

    first commit

--- cloud02/common/src/main/java/len/cloud02/common/Util/LenText.java
+++ cloud02/common/src/main/java/len/cloud02/common/Util/LenText.java
@@ -2,7 +2,10 @@ package len.cloud02.common.Util;
 
 public class LenText {
     public static String makeTextUseful(String str){
-        str = str.replace(" ", "").replace("/", "");
+        String[] dangerSign = new String[]{"/", "-"};
+        for (String s : dangerSign) {
+            str = str.replace(s, "");
+        }
         return str;
     }
 }
