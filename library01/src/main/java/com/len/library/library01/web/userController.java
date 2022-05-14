c2f72c46d5cd450429fa9d5c85b558347f0e7916
boyLenGit <1024552631@qq.com>
2022-05-14 20:54:23 +0800

    first commit

--- library01/src/main/java/com/len/library/library01/web/userController.java
+++ library01/src/main/java/com/len/library/library01/web/userController.java
@@ -1,5 +1,6 @@
 package com.len.library.library01.web;
 
+import com.len.library.library01.pojo.Book;
 import com.len.library.library01.pojo.User;
 import com.len.library.library01.service.BookService;
 import com.len.library.library01.service.UserService;
@@ -9,10 +10,7 @@ import com.len.library.library01.util.LenTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -70,6 +68,26 @@ public class userController {
             user.setPassword(null);
             session.setAttribute("user", user);
             return "redirect:/";
+        }
+    }
+
+    @GetMapping("/sign_out")
+    public String sign_out(HttpSession httpSession){
+        httpSession.removeAttribute("user");
+        return "redirect:/";
+    }
+
+    // 借阅
+    @GetMapping("/borrow/{id}")
+    public String borrow(@PathVariable Integer id, RedirectAttributes attributes){
+        Book book = bookService.getBookById(id);
+        if (book.getRemain()>0){
+            book.setRemain(book.getRemain()-1);
+            attributes.addFlashAttribute("message_borrow", "书籍《 " + book.getName() + "》借阅成功！");
+            return "redirect:/books";
+        }else {
+            attributes.addFlashAttribute("message_borrow", "书籍《 " + book.getName() + "》已经全被借走了！");
+            return "redirect:/books";
         }
     }
 }
