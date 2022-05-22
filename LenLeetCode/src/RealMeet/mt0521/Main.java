9fef105d1e659faea1fdbc74b3687ceb32207585
boyLenGit <1024552631@qq.com>
2022-05-22 14:39:22 +0800

    first commit

--- LenLeetCode/src/RealMeet/mt0521/Main.java
+++ LenLeetCode/src/RealMeet/mt0521/Main.java
@@ -11,6 +11,18 @@ import java.util.Scanner;
 // 123 234
 // 1234
 
+// 123
+// 1 2 3
+// 12 23 13
+// 123
+
+// 12345
+// 1 2 3 4 5  x5
+// 12 13 14 15 23 24 25 34 35 45  x10
+// 123 234 345 135 x4
+// 1234 2345
+// 12345
+
 public class Main {
     public static void main(String[] args) {
         Solution0521 solution0521 = new Solution0521();
@@ -18,73 +30,54 @@ public class Main {
         int tree_num = Integer.parseInt(scanner.nextLine());
         String str = scanner.nextLine();
         String[] strings = str.split(" ");
-        int[] trees = new int[strings.length];
-        System.out.println(solution0521.howMany_multy(trees));
-//        System.out.println(solution0521.categorys(2,5));
+
+        if (strings.length==1) System.out.println(1);
+        else {
+            int[] trees = new int[strings.length];
+            for (int i1=0; i1<strings.length; i1++){
+                trees[i1] = Integer.parseInt(strings[i1]);
+            }
+            System.out.println(solution0521.howMany(trees));
+        }
     }
 }
 
 class Solution0521{
     public int howMany(int[] trees){
-        int res = trees.length;
-        // 树>3  1 3 4 5
-        for (int i1=2; i1<trees.length; i1++){
-            res += categorys(i1, trees.length);
-        }
-        return res;
-    }
-
-    // 1 2 3 4 5 -> 1 3 5
-    public int howMany_multy(int[] trees){
         int res = trees.length + categorys(2, trees.length);
-        // 树>3  1 3 4 5
-        int length = 3, distance = 1;
+        int length = 3;
         int sub = 0;
         boolean isBreak;
-        for (int i1=0; i1+length*distance<=trees.length; i1++){
-            isBreak = false;
-            int debug1 = i1+length*distance;
-            if (i1+length*distance<=trees.length){
-                for (int i2=i1+distance; i2<i1+length*distance; i2+=distance){
-                    if (i2==i1+distance) sub = trees[i2] - trees[i1];
-                    else {
-                        if ((trees[i2]-trees[i2+distance])!=sub) {
-                            isBreak = true;
-                            break;
+        for (int distance=1; length*distance<=trees.length; distance++){
+            for (int i1=0; i1+length*distance<=trees.length; i1++){
+                isBreak = false;
+                if (i1+length*distance<=trees.length){
+                    for (int i2=i1+distance; i2<i1+length*distance; i2+=distance){
+                        if (i2==i1+distance) {
+                            sub = trees[i2] - trees[i1];
+                        }
+                        else {
+                            if ((trees[i2]-trees[i2-distance])!=sub) {
+                                isBreak = true;
+                                break;
+                            }
                         }
                     }
+                    if (!isBreak) res++; // 表示符合等差
                 }
-                if (isBreak==false) res++; // 表示符合等差
-            }else {
-                i1 = 0;
-                length++;
+                if (i1+length*distance + 1 > trees.length){
+                    i1 = -1;
+                    length++;
+                }
             }
         }
         return res;
     }
 
-//    public int numberOfArithmeticSlices(int[] nums) {
-//        int ans=0;
-//        Map<Long,Integer>[] mapArr=new HashMap[nums.length];
-//        for(int i=0;i<nums.length;i++){
-//            Map<Long,Integer> map=new HashMap<>();
-//            for(int j=0;j<i;j++){
-//                long diff=(long)nums[i]-nums[j];
-//                map.put(diff,map.getOrDefault(diff,0)+mapArr[j].getOrDefault(diff,0)+1);
-//                ans+=mapArr[j].getOrDefault(diff,0);
-//            }
-//            mapArr[i]=map;
-//        }
-//        return ans;
-//    }
-
+    // 求排列组合: C{n, m}
     public int categorys(int n, int m){
         ArrayList<Integer> nums_m = new ArrayList<>();
         ArrayList<Integer> nums_n = new ArrayList<>();
-        // n!
-//        for (int i1=n; i1<=n; i1++){
-//            nums_n.add(i1);
-//        }
         // m-n!
         for (int i1=2; i1<=(m-n); i1++){
             nums_n.add(i1);
@@ -94,7 +87,7 @@ class Solution0521{
             nums_m.add(i1);
         }
         int res = 1;
-        // 除
+        // 阶乘求C{n, m}
         int i_n = 0;
         for (int i_m=0; i_m<nums_m.size();i_m++){
             res *= nums_m.get(i_m);
--- LenLeetCode/src/leet/l746.java
+++ LenLeetCode/src/leet/l746.java
@@ -7,7 +7,6 @@ public class l746 {
     public static void main(String[] args) {
         Solution746 solution746 = new Solution746();
 //        System.out.println(solution746.minCostClimbingStairs(new int[]{1,100,1,1,1,100,1,1,100,1}));
-        System.out.println(Solution746.momo());
     }
 }
 
@@ -41,13 +40,4 @@ class Solution746 {
         return Math.min(res0, temp);
     }
 
-    public static int momo(){
-        String string = "123456";
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i1=0; i1<string.length(); i1=i1+2){
-            list.add(Integer.valueOf(string.substring(i1, i1+2)));
-        }
-        System.out.println(Arrays.toString(list.toArray()));
-        return 0;
-    }
 }
