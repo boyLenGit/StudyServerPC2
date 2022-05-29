621b381bca95934ff80c2075d00857312db2b199
boyLenGit <1024552631@qq.com>
2022-05-29 20:36:15 +0800

    first commit

--- LenLeetCode/src/leet/l448.java
+++ LenLeetCode/src/leet/l448.java
@@ -3,11 +3,11 @@ package leet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+//[1,1,2,2]
 public class l448 {
     public static void main(String[] args) {
-        int[] in1 = new int[]{4,3,2,7,8,2,3,1};
-        System.out.println(Arrays.toString(findDisappearedNumbers(in1).toArray()));
+        int[] in1 = new int[]{1,1,2,2};
+        System.out.println(Arrays.toString(findDisappearedNumbers2(in1).toArray()));
     }
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
@@ -16,14 +16,19 @@ public class l448 {
         quicksort(nums, 0, nums.length-1);
         for (int i1=nums.length-1; i1>=0; i1--){
             if (nums[i1]>length){
-                while (nums[i1]>=length) i1--;
+                while (i1>0 && nums[i1-1]>=length) i1--;
+                if (nums[i1]==length) length--;
             }else if (nums[i1]==length){
-                while (nums[i1]==length) i1--;
+                while (i1>0 && nums[i1-1]==length) i1--;
                 length--;
             }else {
-                res.add(nums[i1]);
+                while (nums[i1]<length) {
+                    res.add(length--);
+                }
+                if (nums[i1]==length) length--;
             }
         }
+        while (length!=0) res.add(length--);
         return res;
     }
 
@@ -44,5 +49,17 @@ public class l448 {
         list[s] = compare;
         quicksort(list, start, e-1);
         quicksort(list, e+1, end);
+    }
+
+    // 力扣评论区方法
+    public static List<Integer> findDisappearedNumbers2(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i1=0; i1<nums.length; i1++){
+            if (nums[i1]<nums.length && nums[nums[i1]]>0) nums[nums[i1]] = -nums[nums[i1]];
+        }
+        for (int i1=0; i1<nums.length; i1++){
+            if (nums[i1]>=0) res.add(i1+1);
+        }
+        return res;
     }
 }
